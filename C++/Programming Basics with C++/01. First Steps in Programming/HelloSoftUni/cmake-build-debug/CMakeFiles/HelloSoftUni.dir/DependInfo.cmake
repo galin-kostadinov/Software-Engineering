aff9f6d972fb8223af845c36dc11cfc5cd161986
Galin Kostadinov <gk_1988@abv.bg>
2019-07-18 23:01:57 +0300

    Add Programming Basics with C++/01. First Steps in Programming - All tasks

---
+++ C++/Programming Basics with C++/01. First Steps in Programming/HelloSoftUni/cmake-build-debug/CMakeFiles/HelloSoftUni.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "E:/ComputerProgramming/C++/Programming Basics with C++ 062019/04. First Steps in Programing/tasks/HelloSoftUni/main.cpp" "E:/ComputerProgramming/C++/Programming Basics with C++ 062019/04. First Steps in Programing/tasks/HelloSoftUni/cmake-build-debug/CMakeFiles/HelloSoftUni.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
