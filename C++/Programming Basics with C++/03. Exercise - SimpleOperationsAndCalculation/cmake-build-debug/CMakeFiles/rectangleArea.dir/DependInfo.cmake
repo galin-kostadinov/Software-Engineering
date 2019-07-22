943e806ef665aff97543161715d608b5639e5e87
Galin Kostadinov <gk_1988@abv.bg>
2019-07-22 22:19:44 +0300

    Add 03. Exercise - SimpleOperationsAndCalculation

---
+++ C++/Programming Basics with C++/03. Exercise - SimpleOperationsAndCalculation/cmake-build-debug/CMakeFiles/rectangleArea.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "E:/ComputerProgramming/C++/Programming Basics with C++ 062019/06. Exercise - SimpleOperationsAndCalculation/tasks/simple_tasks/rectangleArea.cpp" "E:/ComputerProgramming/C++/Programming Basics with C++ 062019/06. Exercise - SimpleOperationsAndCalculation/tasks/cmake-build-debug/CMakeFiles/rectangleArea.dir/simple_tasks/rectangleArea.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ C++/Programming Basics with C++/03. Exercise - SimpleOperationsAndCalculation/cmake-build-debug/CMakeFiles/tasks.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tasks.dir/main.cpp.obj"
+  "CMakeFiles/tasks.dir/simple_tasks/convertorUSDtoBGN.cpp.obj"
+  "tasks.pdb"
+  "tasks.exe"
+  "tasks.exe.manifest"
+  "libtasks.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tasks.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
