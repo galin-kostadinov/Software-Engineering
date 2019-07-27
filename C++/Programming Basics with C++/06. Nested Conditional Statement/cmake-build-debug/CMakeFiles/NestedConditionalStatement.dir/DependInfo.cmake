1dbd9e1e134061d96608cb26a98835acd98e4eda
Galin Kostadinov <gk_1988@abv.bg>
2019-07-27 12:01:14 +0300

    Add 06. Nested Conditional Statement

---
+++ C++/Programming Basics with C++/06. Nested Conditional Statement/cmake-build-debug/CMakeFiles/NestedConditionalStatement.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "E:/ComputerProgramming/C++/Programming Basics with C++ 062019/09. NestedConditionalStatement/tasks/NestedConditionalStatement/main.cpp" "E:/ComputerProgramming/C++/Programming Basics with C++ 062019/09. NestedConditionalStatement/tasks/NestedConditionalStatement/cmake-build-debug/CMakeFiles/NestedConditionalStatement.dir/main.cpp.obj"
+  "E:/ComputerProgramming/C++/Programming Basics with C++ 062019/09. NestedConditionalStatement/tasks/NestedConditionalStatement/simple_task/FruitOrVegetable.cpp" "E:/ComputerProgramming/C++/Programming Basics with C++ 062019/09. NestedConditionalStatement/tasks/NestedConditionalStatement/cmake-build-debug/CMakeFiles/NestedConditionalStatement.dir/simple_task/FruitOrVegetable.cpp.obj"
+  "E:/ComputerProgramming/C++/Programming Basics with C++ 062019/09. NestedConditionalStatement/tasks/NestedConditionalStatement/simple_task/FruitShop.cpp" "E:/ComputerProgramming/C++/Programming Basics with C++ 062019/09. NestedConditionalStatement/tasks/NestedConditionalStatement/cmake-build-debug/CMakeFiles/NestedConditionalStatement.dir/simple_task/FruitShop.cpp.obj"
+  "E:/ComputerProgramming/C++/Programming Basics with C++ 062019/09. NestedConditionalStatement/tasks/NestedConditionalStatement/simple_task/InvalidNumber.cpp" "E:/ComputerProgramming/C++/Programming Basics with C++ 062019/09. NestedConditionalStatement/tasks/NestedConditionalStatement/cmake-build-debug/CMakeFiles/NestedConditionalStatement.dir/simple_task/InvalidNumber.cpp.obj"
+  "E:/ComputerProgramming/C++/Programming Basics with C++ 062019/09. NestedConditionalStatement/tasks/NestedConditionalStatement/simple_task/PersonalTitles.cpp" "E:/ComputerProgramming/C++/Programming Basics with C++ 062019/09. NestedConditionalStatement/tasks/NestedConditionalStatement/cmake-build-debug/CMakeFiles/NestedConditionalStatement.dir/simple_task/PersonalTitles.cpp.obj"
+  "E:/ComputerProgramming/C++/Programming Basics with C++ 062019/09. NestedConditionalStatement/tasks/NestedConditionalStatement/simple_task/SkiTrip.cpp" "E:/ComputerProgramming/C++/Programming Basics with C++ 062019/09. NestedConditionalStatement/tasks/NestedConditionalStatement/cmake-build-debug/CMakeFiles/NestedConditionalStatement.dir/simple_task/SkiTrip.cpp.obj"
+  "E:/ComputerProgramming/C++/Programming Basics with C++ 062019/09. NestedConditionalStatement/tasks/NestedConditionalStatement/simple_task/SmallShop.cpp" "E:/ComputerProgramming/C++/Programming Basics with C++ 062019/09. NestedConditionalStatement/tasks/NestedConditionalStatement/cmake-build-debug/CMakeFiles/NestedConditionalStatement.dir/simple_task/SmallShop.cpp.obj"
+  "E:/ComputerProgramming/C++/Programming Basics with C++ 062019/09. NestedConditionalStatement/tasks/NestedConditionalStatement/simple_task/TradeCommissions.cpp" "E:/ComputerProgramming/C++/Programming Basics with C++ 062019/09. NestedConditionalStatement/tasks/NestedConditionalStatement/cmake-build-debug/CMakeFiles/NestedConditionalStatement.dir/simple_task/TradeCommissions.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ C++/Programming Basics with C++/06. Nested Conditional Statement/cmake-build-debug/CMakeFiles/PersonalTitles.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "E:/ComputerProgramming/C++/Programming Basics with C++ 062019/09. NestedConditionalStatement/tasks/NestedConditionalStatement/simple_task/PersonalTitles.cpp" "E:/ComputerProgramming/C++/Programming Basics with C++ 062019/09. NestedConditionalStatement/tasks/NestedConditionalStatement/cmake-build-debug/CMakeFiles/PersonalTitles.dir/simple_task/PersonalTitles.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
