4b8057a732b6b55f5ae433a515c5fe7b9f6ff85e
Galin Kostadinov <gk_1988@abv.bg>
2019-07-27 23:54:11 +0300

    Add AssociativeArraysMoreExercise/MOBAChallenger.java Update AssociativeArraysMoreExercise/Judge.java

---
+++ Java/Technology Fundamentals with Java/Exercises Java Code/src/AssociativeArraysMoreExercise/MOBAChallenger.java
@@ -0,0 +1,12 @@
+package AssociativeArraysMoreExercise;
+
+import java.util.Scanner;
+
+public class MOBAChallenger {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+    }
+}
+
