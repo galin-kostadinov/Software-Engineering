8595771d7e534ac1ae567767c91a414527c1289a
Galin Kostadinov <gk_1988@abv.bg>
2019-07-30 00:29:34 +0300

    Add CarSalesmanMoreEx/Car.java Add CarSalesmanMoreEx/Engine.java Add CarSalesmanMoreEx/Main.java

---
+++ Java/Technology Fundamentals with Java/Object And Classes Exsercise/src/CarSalesmanMoreEx/Car.java
@@ -0,0 +1,8 @@
+package CarSalesmanMoreEx;
+
+public class Car {
+    private String model;
+    private Engine engine;
+    private int weight;
+    private String color;
+}
---
+++ Java/Technology Fundamentals with Java/Object And Classes Exsercise/src/CarSalesmanMoreEx/Engine.java
@@ -0,0 +1,9 @@
+package CarSalesmanMoreEx;
+
+public class Engine {
+    private String model;
+    private int power;
+    private int displacement;
+    private String efficiency;
+
+}
