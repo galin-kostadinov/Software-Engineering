3bc7bbef366c38290eb14955a18b39c6043cf35a
Galin Kostadinov <gk_1988@abv.bg>
2019-07-30 13:19:45 +0300

    Update CarSalesmanMoreEx/Car.java Update CarSalesmanMoreEx/Engine.java Update CarSalesmanMoreEx/Main.java

--- Java/Technology Fundamentals with Java/Object And Classes Exsercise/src/CarSalesmanMoreEx/Main.java
+++ Java/Technology Fundamentals with Java/Object And Classes Exsercise/src/CarSalesmanMoreEx/Main.java
@@ -1,14 +1,25 @@
 package CarSalesmanMoreEx;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int count = Integer.parseInt(sc.nextLine());
 
-        for (int i = 0; i <count ; i++) {
+        int n = Integer.parseInt(sc.nextLine());
+        List<Engine> engineList = new ArrayList<>(n);
+
+        for (int i = 0; i < n; i++) {
+
+        }
+
+        int m = Integer.parseInt(sc.nextLine());
+        List<Car> carList = new ArrayList<>(m);
+
+        for (int i = 0; i < m; i++) {
 
         }
     }
