de97d95b0473377c07d8fdeaa25cb4008bcf291f
Galin Kostadinov <gk_1988@abv.bg>
2019-07-31 12:07:57 +0300

    Update GreaterNumber.cpp

--- C++/Programming Basics with C++/04. Conditional statements/simple_tasks/GreaterNumber.cpp
+++ C++/Programming Basics with C++/04. Conditional statements/simple_tasks/GreaterNumber.cpp
@@ -4,11 +4,11 @@ using namespace std;
 
 int main() {
     int num1, num2;
-    cin >> num1>> num2;
+    cin >> num1 >> num2;
 
     if (num1 > num2) {
         cout << num1 << endl;
-    }else{
+    } else {
         cout << num2 << endl;
     }
 
