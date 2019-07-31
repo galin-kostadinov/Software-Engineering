05863c6eceb7b67c71fe182efcaf6629c1c08cf5
Galin Kostadinov <gk_1988@abv.bg>
2019-07-31 12:27:48 +0300

    Update Volleyball.cpp

--- C++/Programming Basics with C++/07. Exercise - Nested Conditional Statement/simple_task/Volleyball.cpp
+++ C++/Programming Basics with C++/07. Exercise - Nested Conditional Statement/simple_task/Volleyball.cpp
@@ -6,23 +6,25 @@
 using namespace std;
 
 int main() {
-
     string typeOfYear;
     int holidays;
     int weekendTripCount;
     cin >> typeOfYear >> holidays >> weekendTripCount;
+
     transform(typeOfYear.begin(),
               typeOfYear.end(),
               typeOfYear.begin(),
               ::tolower);
 
     double numberSportingDays = 0;
+
     if (typeOfYear == "normal") {
         numberSportingDays = weekendTripCount + (48 - weekendTripCount) * 3 / 4.0 + holidays * 2 / 3.0;
         cout << floor(numberSportingDays) << endl;
     } else if (typeOfYear == "leap") {
         numberSportingDays = (weekendTripCount + (48 - weekendTripCount) * 3 / 4.0 + holidays * 2 / 3.0) * 1.15;
-        cout << floor(numberSportingDays)<<endl;
+        cout << floor(numberSportingDays) << endl;
     }
+
     return 0;
 }
