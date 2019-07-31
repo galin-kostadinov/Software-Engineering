b6ebe3aff5489e651dad0e218ed92857f05bebaf
Galin Kostadinov <gk_1988@abv.bg>
2019-07-31 12:24:55 +0300

    Update OnTimeForTheExam.cpp

--- C++/Programming Basics with C++/07. Exercise - Nested Conditional Statement/simple_task/OnTimeForTheExam.cpp
+++ C++/Programming Basics with C++/07. Exercise - Nested Conditional Statement/simple_task/OnTimeForTheExam.cpp
@@ -3,18 +3,18 @@
 using namespace std;
 
 int main() {
-
     int examHour;
     int examMinute;
     int arrivalHour;
     int arrivalMinute;
 
     cin >> examHour >> examMinute >> arrivalHour >> arrivalMinute;
+
     examMinute = examMinute + examHour * 60;
     arrivalMinute = arrivalMinute + arrivalHour * 60;
+
     int minuteLeft = 0;
     int hourLeft = 0;
-
 
     if (examMinute >= arrivalMinute && (examMinute - arrivalMinute) <= 30) {
         cout << "On time" << endl;
@@ -24,12 +24,12 @@ int main() {
         cout << "Late" << endl;
     }
 
-
     if (examMinute > arrivalMinute && (examMinute - arrivalMinute) < 60) {
         cout << examMinute - arrivalMinute << " minutes before the start" << endl;
     } else if ((examMinute - arrivalMinute) >= 60) {
         hourLeft = (examMinute - arrivalMinute) / 60;
         minuteLeft = (examMinute - arrivalMinute) % 60;
+
         if (minuteLeft <= 9) {
             cout << hourLeft << ":0" << minuteLeft << " hours before the start" << endl;
         } else {
@@ -42,6 +42,7 @@ int main() {
     } else if ((arrivalMinute - examMinute) >= 60) {
         hourLeft = (arrivalMinute - examMinute) / 60;
         minuteLeft = (arrivalMinute - examMinute) % 60;
+
         if (minuteLeft <= 9) {
             cout << hourLeft << ":0" << minuteLeft << " hours after the start" << endl;
         } else {
