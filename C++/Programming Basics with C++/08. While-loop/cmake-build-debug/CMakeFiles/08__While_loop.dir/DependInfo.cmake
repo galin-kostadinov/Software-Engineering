4f7978a0a1cee9e51f5eeaf6a7955fd6367d9968
Galin Kostadinov <gk_1988@abv.bg>
2019-07-31 16:32:37 +0300

    Add 08. While-loop

---
+++ C++/Programming Basics with C++/08. While-loop/cmake-build-debug/CMakeFiles/08__While_loop.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "E:/ComputerProgramming/GitHubRepoSoftUniTasks/Software-Engineering/C++/Programming Basics with C++/08. While-loop/simple_tasks/AccountBalance.cpp" "E:/ComputerProgramming/GitHubRepoSoftUniTasks/Software-Engineering/C++/Programming Basics with C++/08. While-loop/cmake-build-debug/CMakeFiles/08__While_loop.dir/simple_tasks/AccountBalance.cpp.obj"
+  "E:/ComputerProgramming/GitHubRepoSoftUniTasks/Software-Engineering/C++/Programming Basics with C++/08. While-loop/simple_tasks/Graduation.cpp" "E:/ComputerProgramming/GitHubRepoSoftUniTasks/Software-Engineering/C++/Programming Basics with C++/08. While-loop/cmake-build-debug/CMakeFiles/08__While_loop.dir/simple_tasks/Graduation.cpp.obj"
+  "E:/ComputerProgramming/GitHubRepoSoftUniTasks/Software-Engineering/C++/Programming Basics with C++/08. While-loop/simple_tasks/GraduationPt2.cpp" "E:/ComputerProgramming/GitHubRepoSoftUniTasks/Software-Engineering/C++/Programming Basics with C++/08. While-loop/cmake-build-debug/CMakeFiles/08__While_loop.dir/simple_tasks/GraduationPt2.cpp.obj"
+  "E:/ComputerProgramming/GitHubRepoSoftUniTasks/Software-Engineering/C++/Programming Basics with C++/08. While-loop/simple_tasks/MaxNumber.cpp" "E:/ComputerProgramming/GitHubRepoSoftUniTasks/Software-Engineering/C++/Programming Basics with C++/08. While-loop/cmake-build-debug/CMakeFiles/08__While_loop.dir/simple_tasks/MaxNumber.cpp.obj"
+  "E:/ComputerProgramming/GitHubRepoSoftUniTasks/Software-Engineering/C++/Programming Basics with C++/08. While-loop/simple_tasks/MinNumber.cpp" "E:/ComputerProgramming/GitHubRepoSoftUniTasks/Software-Engineering/C++/Programming Basics with C++/08. While-loop/cmake-build-debug/CMakeFiles/08__While_loop.dir/simple_tasks/MinNumber.cpp.obj"
+  "E:/ComputerProgramming/GitHubRepoSoftUniTasks/Software-Engineering/C++/Programming Basics with C++/08. While-loop/simple_tasks/Moving.cpp" "E:/ComputerProgramming/GitHubRepoSoftUniTasks/Software-Engineering/C++/Programming Basics with C++/08. While-loop/cmake-build-debug/CMakeFiles/08__While_loop.dir/simple_tasks/Moving.cpp.obj"
+  "E:/ComputerProgramming/GitHubRepoSoftUniTasks/Software-Engineering/C++/Programming Basics with C++/08. While-loop/simple_tasks/NumberInRange1to100.cpp" "E:/ComputerProgramming/GitHubRepoSoftUniTasks/Software-Engineering/C++/Programming Basics with C++/08. While-loop/cmake-build-debug/CMakeFiles/08__While_loop.dir/simple_tasks/NumberInRange1to100.cpp.obj"
+  "E:/ComputerProgramming/GitHubRepoSoftUniTasks/Software-Engineering/C++/Programming Basics with C++/08. While-loop/simple_tasks/Sequence2k-plus-1.cpp" "E:/ComputerProgramming/GitHubRepoSoftUniTasks/Software-Engineering/C++/Programming Basics with C++/08. While-loop/cmake-build-debug/CMakeFiles/08__While_loop.dir/simple_tasks/Sequence2k-plus-1.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ C++/Programming Basics with C++/08. While-loop/cmake-build-debug/CMakeFiles/08__While_loop.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/08__While_loop.dir/simple_tasks/AccountBalance.cpp.obj"
+  "CMakeFiles/08__While_loop.dir/simple_tasks/Graduation.cpp.obj"
+  "CMakeFiles/08__While_loop.dir/simple_tasks/GraduationPt2.cpp.obj"
+  "CMakeFiles/08__While_loop.dir/simple_tasks/MaxNumber.cpp.obj"
+  "CMakeFiles/08__While_loop.dir/simple_tasks/MinNumber.cpp.obj"
+  "CMakeFiles/08__While_loop.dir/simple_tasks/Moving.cpp.obj"
+  "CMakeFiles/08__While_loop.dir/simple_tasks/NumberInRange1to100.cpp.obj"
+  "CMakeFiles/08__While_loop.dir/simple_tasks/Sequence2k-plus-1.cpp.obj"
+  "08__While_loop.pdb"
+  "08__While_loop.exe"
+  "08__While_loop.exe.manifest"
+  "lib08__While_loop.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/08__While_loop.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
