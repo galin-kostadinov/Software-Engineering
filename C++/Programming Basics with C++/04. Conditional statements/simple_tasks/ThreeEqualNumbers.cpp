4360ce1c79fb3ba04f0a17c9bdd77256cd66b87d
Galin Kostadinov <gk_1988@abv.bg>
2019-07-31 12:09:02 +0300

    Update ThreeEqualNumbers.cpp

--- C++/Programming Basics with C++/04. Conditional statements/simple_tasks/ThreeEqualNumbers.cpp
+++ C++/Programming Basics with C++/04. Conditional statements/simple_tasks/ThreeEqualNumbers.cpp
@@ -6,7 +6,7 @@ int main() {
     int num1, num2, num3;
     cin >> num1 >> num2 >> num3;
 
-    if (num1==num2 && num2==num3) {
+    if (num1 == num2 && num2 == num3) {
         cout << "yes" << endl;
     } else {
         cout << "no" << endl;
