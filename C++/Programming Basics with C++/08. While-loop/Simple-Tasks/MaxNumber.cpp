2d54692467280b1008533070fe142f2c9b084851
Galin Kostadinov <gk_1988@abv.bg>
2019-07-31 13:37:28 +0300

    Changed arrangement MaxNumber.cpp

--- C++/Programming Basics with C++/08. While-loop/Simple-Tasks/MaxNumber.cpp
+++ C++/Programming Basics with C++/08. While-loop/Simple-Tasks/MaxNumber.cpp
@@ -5,16 +5,22 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
+
     int counter = 0;
     int maxNumber = INT_MIN;
+
     while (counter < n) {
         int number;
         cin >> number;
+
         if (number > maxNumber) {
             maxNumber = number;
         }
+       
         counter++;
     }
+
     cout << maxNumber << endl;
+
     return 0;
 }
