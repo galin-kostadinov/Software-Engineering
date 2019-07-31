84669b484acc9ec62db353b1f23f83d9d4e7cd3b
Galin Kostadinov <gk_1988@abv.bg>
2019-07-31 12:31:45 +0300

    Update AccountBalance.cpp

--- C++/Programming Basics with C++/08. While-loop/Simple-Tasks/AccountBalance.cpp
+++ C++/Programming Basics with C++/08. While-loop/Simple-Tasks/AccountBalance.cpp
@@ -5,21 +5,28 @@ using namespace std;
 int main() {
     int number;
     cin >> number;
+
     int counter = 0;
     double accountBalance = 0;
+
     cout.setf(ios::fixed);
     cout.precision(2);
+
     while (counter < number) {
         double currentAmount;
         cin >> currentAmount;
+
         if(currentAmount<0){
             cout<<"Invalid operation!"<<endl;
             break;
         }
+
         accountBalance += currentAmount;
         cout << "Increase: " << currentAmount << endl;
         counter++;
     }
+
     cout << "Total: " << accountBalance << endl;
+
     return 0;
 }
