4cbb3d6aa34b1e1c2b7b269ea5c4ae58befdfcb7
Galin Kostadinov <gk_1988@abv.bg>
2019-07-31 13:35:19 +0300

    Changed arrangement Graduation.cpp

--- C++/Programming Basics with C++/08. While-loop/Simple-Tasks/Graduation.cpp
+++ C++/Programming Basics with C++/08. While-loop/Simple-Tasks/Graduation.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main() {
     string name;
-    cin>>name;
+    cin >> name;
 
     double totalSum = 0;
     int counter = 1;
@@ -14,15 +14,17 @@ int main() {
     while (counter <= 12) {
         double currentGrade;
         cin >> currentGrade;
-        if(currentGrade>=4){
+
+        if (currentGrade >= 4) {
             totalSum += currentGrade;
             counter++;
         }
-
     }
 
     cout.setf(ios::fixed);
     cout.precision(2);
+
     cout << name << " graduated. Average grade: " << totalSum / 12 << endl;
+
     return 0;
 }
