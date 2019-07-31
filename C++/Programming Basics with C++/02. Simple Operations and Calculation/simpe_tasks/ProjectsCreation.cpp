00769e9252bad00c1b9595b444c6338ecf6bcb68
Galin Kostadinov <gk_1988@abv.bg>
2019-07-31 11:57:11 +0300

    Update ProjectsCreation.cpp

--- C++/Programming Basics with C++/02. Simple Operations and Calculation/simpe_tasks/ProjectsCreation.cpp
+++ C++/Programming Basics with C++/02. Simple Operations and Calculation/simpe_tasks/ProjectsCreation.cpp
@@ -5,7 +5,9 @@ using namespace std;
 int main() {
     string architect;
     int projectNum;
+
     cin >> architect >> projectNum;
+
     int hours = projectNum * 3;
 
     cout << "The architect " << architect << " will need " << hours
