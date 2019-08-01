f1f417cf25268fad7782e412c7ecf93013567d9c
Galin Kostadinov <gk_1988@abv.bg>
2019-08-01 13:21:09 +0300

    Update TheIsleOfManTTRace.java

--- Java/Technology Fundamentals with Java/Final Exam Preparation/src/FinalExam14April2019GroupII/TheIsleOfManTTRace.java
+++ Java/Technology Fundamentals with Java/Final Exam Preparation/src/FinalExam14April2019GroupII/TheIsleOfManTTRace.java
@@ -8,16 +8,18 @@ public class TheIsleOfManTTRace {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String regex = "([#$%*&]{1})(?<name>[A-Za-z]+)\\1=(?<length>[0-9]+)!!(?<encryptedGeohashCode>.+)";
+        String regex = "^([#$%*&]{1})(?<name>[A-Za-z]+)\\1=(?<length>[0-9]+)!!(?<encryptedGeohashCode>.+)$";
         Pattern pattern = Pattern.compile(regex);
 
         while (true) {
             String input = sc.nextLine();
+
             Matcher matcher = pattern.matcher(input);
             String name = "";
             int length = 0;
             String encryptedGeohashCode = "";
             boolean isFound = false;
+
             if (matcher.find()) {
                 name = matcher.group("name");
                 length = Integer.parseInt(matcher.group("length"));
@@ -35,6 +37,7 @@ public class TheIsleOfManTTRace {
                 }
 
                 System.out.printf("Coordinates found! %s -> %s", name, geohashCode);
+
                 return;
             } else {
                 System.out.println("Nothing found!");
