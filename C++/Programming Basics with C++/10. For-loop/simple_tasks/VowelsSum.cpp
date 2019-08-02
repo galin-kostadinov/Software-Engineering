f234052cf280435353eef6fcc72af6301aecaead
Galin Kostadinov <gk_1988@abv.bg>
2019-08-02 17:37:51 +0300

    Update VowelsSum.cpp

--- C++/Programming Basics with C++/10. For-loop/simple_tasks/VowelsSum.cpp
+++ C++/Programming Basics with C++/10. For-loop/simple_tasks/VowelsSum.cpp
@@ -6,9 +6,12 @@ using namespace std;
 int main() {
     string text;
     getline(cin, text);
+
     int sum = 0;
+
     for (int i = 0; i < text.length(); i++) {
         char symbol = text[i];
+
         if (symbol == 'a') {
             sum += 1;
         } else if (symbol == 'e') {
@@ -21,6 +24,7 @@ int main() {
             sum += 5;
         }
     }
+
     cout << sum << endl;
 
     return 0;
