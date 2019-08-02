dcf95f4cea9e7f33b543fe5dd6b576296bcd6027
Galin Kostadinov <gk_1988@abv.bg>
2019-08-02 17:35:27 +0300

    Update NumberSequence.cpp

--- C++/Programming Basics with C++/10. For-loop/simple_tasks/NumberSequence.cpp
+++ C++/Programming Basics with C++/10. For-loop/simple_tasks/NumberSequence.cpp
@@ -5,22 +5,27 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    int sum = 0;
-    int min;
-    int max;
+
     int num;
     cin >> num;
+
+    int min;
+    int max;
     min = num;
     max = num;
+
     for (int i = 0; i < n - 1; i++) {
         cin >> num;
+
         if (num < min) {
             min = num;
         }
+
         if (num > max) {
             max = num;
         }
     }
+
     cout << "Max number: " << max << endl;
     cout << "Min number: " << min << endl;
 
