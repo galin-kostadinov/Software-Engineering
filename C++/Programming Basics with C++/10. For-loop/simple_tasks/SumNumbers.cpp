81b3a04facff06b278f88bfeb53eb6be5be28746
Galin Kostadinov <gk_1988@abv.bg>
2019-08-02 17:37:09 +0300

    Update SumNumbers.cpp

--- C++/Programming Basics with C++/10. For-loop/simple_tasks/SumNumbers.cpp
+++ C++/Programming Basics with C++/10. For-loop/simple_tasks/SumNumbers.cpp
@@ -5,12 +5,16 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
+
     int sum = 0;
+
     for (int i = 0; i < n; ++i) {
         int num;
         cin >> num;
         sum += num;
     }
+
     cout << sum << endl;
+
     return 0;
 }
