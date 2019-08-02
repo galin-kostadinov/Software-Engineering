5f0575cab9edcc611be70aefaa15e4cdcd633049
Galin Kostadinov <gk_1988@abv.bg>
2019-08-02 17:33:49 +0300

    Update LeftAndRightSum.cpp

--- C++/Programming Basics with C++/10. For-loop/simple_tasks/LeftAndRightSum.cpp
+++ C++/Programming Basics with C++/10. For-loop/simple_tasks/LeftAndRightSum.cpp
@@ -8,20 +8,24 @@ int main() {
     cin >> n;
     int leftSum = 0;
     int rightSum = 0;
+
     for (int i = 0; i < n; i++) {
         int num;
         cin >> num;
         leftSum += num;
     }
+
     for (int i = 0; i < n; i++) {
         int num;
         cin >> num;
         rightSum += num;
     }
+
     if (leftSum == rightSum) {
         cout << "Yes, sum = " << leftSum << endl;
     } else {
         cout << "No, diff = " << abs(leftSum - rightSum) << endl;
     }
+
     return 0;
 }
