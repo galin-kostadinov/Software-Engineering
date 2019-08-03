04bcece98271798f0b79400a1ad609302e86d4e9
Galin Kostadinov <gk_1988@abv.bg>
2019-08-03 22:08:24 +0300

    Add PrinterQueue.java

---
+++ Java/Java Advanced/Exercises Java Code/src/StacksAndQueues_Lab/PrinterQueue.java
@@ -0,0 +1,31 @@
+package StacksAndQueues_Lab;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class PrinterQueue {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // printingQueue.poll(), printingQueue.offer(), printingQueue.peek();
+        ArrayDeque<String> printingQueue = new ArrayDeque<>();
+
+        String input = "";
+
+        while (!"print".equals(input = sc.nextLine())) {
+            if (input.equals("cancel")) {
+                if (printingQueue.size() > 0) {
+                    System.out.println("Canceled " + printingQueue.poll());
+                }else{
+                    System.out.println("Printer is on standby");
+                }
+            } else {
+                printingQueue.offer(input);
+            }
+        }
+
+        while (printingQueue.size() > 0) {
+            System.out.println(printingQueue.poll());
+        }
+    }
+}
