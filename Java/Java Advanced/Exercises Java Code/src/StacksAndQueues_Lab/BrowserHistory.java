67f08156ee64d6ac4c65875610c0d5ae6cc2c8aa
Galin Kostadinov <gk_1988@abv.bg>
2019-08-03 20:57:55 +0300

    Update BrowserHistory.java

--- Java/Java Advanced/Exercises Java Code/src/StacksAndQueues_Lab/BrowserHistory.java
+++ Java/Java Advanced/Exercises Java Code/src/StacksAndQueues_Lab/BrowserHistory.java
@@ -1,12 +1,28 @@
 package StacksAndQueues_Lab;
 
+import java.util.ArrayDeque;
 import java.util.Scanner;
 
 public class BrowserHistory {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        ArrayDeque<String> url = new ArrayDeque<>();
+        String input = "";
 
+        while (!"Home".equals(input = sc.nextLine())) {
+            if (input.equals("back")) {
+                if (url.size() > 1) {
+                    url.pop();
+                    System.out.println(url.peek());
+                }else{
+                    System.out.println("no previous URLs");
+                }
+            } else {
+                url.push(input);
+                System.out.println(url.peek());
+            }
+        }
     }
 }
 
