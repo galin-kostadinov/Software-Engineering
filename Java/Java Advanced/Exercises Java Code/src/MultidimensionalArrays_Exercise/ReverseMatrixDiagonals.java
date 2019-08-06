b38cfb8595864fe4ddcd8bae1cda3f7d64558659
Galin Kostadinov <gk_1988@abv.bg>
2019-08-07 00:24:11 +0300

    Add ReverseMatrixDiagonals.java

---
+++ Java/Java Advanced/Exercises Java Code/src/MultidimensionalArrays_Exercise/ReverseMatrixDiagonals.java
@@ -0,0 +1,39 @@
+package MultidimensionalArrays_Exercise;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ReverseMatrixDiagonals {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int rows = sc.nextInt();
+        int cols = sc.nextInt();
+        sc.nextLine();
+
+        int[][] matrix = new int[rows][cols];
+
+        for (int row = 0; row < matrix.length; row++) {
+            matrix[row] = Arrays.stream(sc.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+        }
+
+        int row = rows - 1;
+        int col = cols - 1;
+
+        while (row >= 0) {
+            int r = row;
+            int c = col;
+
+            while (c < cols && r >= 0) {
+                System.out.print(matrix[r--][c++] + " ");
+            }
+            System.out.println();
+            col--;
+
+            if (col == -1) {
+                col = 0;
+                row--;
+            }
+        }
+    }
+}
