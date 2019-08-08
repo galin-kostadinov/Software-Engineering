bff0383833dec26c51e13e0e5aab67a0469b48d0
Galin Kostadinov <gk_1988@abv.bg>
2019-08-08 23:29:19 +0300

    Add CopyBytes.java

---
+++ Java/Java Advanced/Streams, Files and Directories/Lab/src/CopyBytes.java
@@ -0,0 +1,34 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class CopyBytes {
+    public static void main(String[] args) {
+        String userDir = System.getProperty("user.dir");
+
+        String pathIn = userDir + "\\res\\input.txt";
+        String pathOut = userDir + "\\res\\output.txt";
+
+        try (FileInputStream fis = new FileInputStream(pathIn);
+             FileOutputStream fos = new FileOutputStream(pathOut)) {
+
+            int oneByte = fis.read();
+
+            while (oneByte >= 0) {
+                if (oneByte == 32 || oneByte == 10) {
+                    fos.write(oneByte);
+                } else {
+                    String byteAsText = String.valueOf(oneByte);
+                    for (int i = 0; i < byteAsText.length(); i++) {
+                        fos.write(byteAsText.charAt(i));
+                    }
+                }
+                oneByte = fis.read();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
