9c0022de97d09f11645d83f4b9e6f9f0f6e32140
Galin Kostadinov <gk_1988@abv.bg>
2019-08-08 22:53:50 +0300

    Add ReadFile.java

---
+++ Java/Java Advanced/Streams, Files and Directories/Lab/src/ReadFile.java
@@ -0,0 +1,24 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class ReadFile {
+    public static void main(String[] args) {
+        String userDir = System.getProperty("user.dir");
+
+        String path = userDir + "\\res\\input.txt";
+
+        try (FileInputStream fis = new FileInputStream(path)) {
+            int oneByte = fis.read();
+            while (oneByte >= 0) {
+                String res = Integer.toBinaryString(oneByte);
+                System.out.print(res + " ");
+                oneByte = fis.read();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
