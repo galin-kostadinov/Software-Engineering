07d2a0974eb90d5306448e65f8a77cc460646bc9
Galin Kostadinov <gk_1988@abv.bg>
2019-08-09 13:54:06 +0300

    Add SerializeCustomObject.java

---
+++ Java/Java Advanced/Streams, Files and Directories/Lab/src/SerializeCustomObject.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SerializeCustomObject {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        String userDir = System.getProperty("user.dir");
+        String path = userDir + "\\res\\output.gal";
+
+        List<String> names = new ArrayList<>();
+        Collections.addAll(names, "Mimi", "Gosho");
+
+        FileOutputStream fos = new FileOutputStream(path);
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+
+        oos.writeObject(names);
+
+        FileInputStream fis = new FileInputStream(path);
+        ObjectInputStream ois = new ObjectInputStream(fis);
+
+        List<String> names2 = (List<String>) ois.readObject();
+        System.out.println(names2);
+    }
+}
