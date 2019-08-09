e7e198486a7396d9e8b09e406aba603b42a0834d
Galin Kostadinov <gk_1988@abv.bg>
2019-08-09 22:06:28 +0300

    Add GetFolderSize.java

---
+++ Java/Java Advanced/Streams, Files and Directories/Exersice/src/CopyPicture.java
@@ -0,0 +1,20 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class CopyPicture {
+    public static void main(String[] args) throws IOException {
+        String picInputPath = "E:\\ComputerProgramming\\pics\\JavaLogo.jpg";
+        String picOutputPath = "E:\\ComputerProgramming\\pics\\JavaLogo-copy.txt";
+        FileInputStream inputStream = new FileInputStream(picInputPath);
+
+        byte[] image = inputStream.readAllBytes();
+
+        FileOutputStream outputStream = new FileOutputStream(picOutputPath);
+
+        outputStream.write(image);
+
+        outputStream.close();
+    }
+}
