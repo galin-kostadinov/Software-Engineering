29bf7300270c385b311a3274ad0eb2b410106aa1
Galin Kostadinov <gk_1988@abv.bg>
2019-08-11 14:32:52 +0300

    Add SumNumbers.java

---
+++ Java/Java Advanced/Exercises Java Code/src/FunctionalProgramming_Lab/SumNumbers.java
@@ -0,0 +1,30 @@
+package FunctionalProgramming_Lab;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class SumNumbers {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] splitInput = reader.readLine().split(", ");
+
+        Function<String, Integer> parse = s -> Integer.parseInt(s);
+
+        List<Integer> numbers = Arrays.stream(splitInput)
+                .map(parse)
+                .collect(Collectors.toList());
+
+        System.out.println("Count = " + numbers.size());
+
+        int sum = numbers.stream()
+                .reduce(0, (a, b) -> a + b);
+
+        System.out.println("Sum = " + sum);
+    }
+}
