b15ba801a0c2144769552565554912749ab3efb0
Galin Kostadinov <gk_1988@abv.bg>
2019-08-11 14:49:02 +0300

    Add CountUppercaseWords.java

---
+++ Java/Java Advanced/Exercises Java Code/src/FunctionalProgramming_Lab/CountUppercaseWords.java
@@ -0,0 +1,26 @@
+package FunctionalProgramming_Lab;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class CountUppercaseWords {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] words = reader.readLine().split("\\s+");
+
+        Predicate<String> isStartingWithCapital = word -> Character.isUpperCase((word.charAt(0)));
+
+        List<String> capitalWords = Arrays.stream(words)
+                .filter(isStartingWithCapital)
+                .collect(Collectors.toList());
+
+        System.out.println(capitalWords.size());
+        capitalWords.forEach(System.out::println);
+    }
+}
