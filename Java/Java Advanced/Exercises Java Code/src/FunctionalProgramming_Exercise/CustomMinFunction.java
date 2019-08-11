36cf38464cbfb72bf8c15e966b56bcce4962f806
Galin Kostadinov <gk_1988@abv.bg>
2019-08-11 20:47:24 +0300

    Add CustomMinFunction.java

---
+++ Java/Java Advanced/Exercises Java Code/src/FunctionalProgramming_Exercise/CustomMinFunction.java
@@ -0,0 +1,24 @@
+package FunctionalProgramming_Exercise;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.function.Function;
+
+public class CustomMinFunction {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] numbers = Arrays.stream(reader.readLine().split("\\s+"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        Function<int[], Integer> minNumber = arr -> {
+            int min = Arrays.stream(arr).min().getAsInt();
+            return min;
+        };
+
+        System.out.println(minNumber.apply(numbers));
+    }
+}
