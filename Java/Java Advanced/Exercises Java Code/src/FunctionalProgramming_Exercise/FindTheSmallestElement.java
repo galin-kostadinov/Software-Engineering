008066a2910e5262701110c92e19f0bdb9ac0b5b
Galin Kostadinov <gk_1988@abv.bg>
2019-08-12 14:59:25 +0300

    Add FindTheSmallestElement.java

---
+++ Java/Java Advanced/Exercises Java Code/src/FunctionalProgramming_Exercise/FindTheSmallestElement.java
@@ -0,0 +1,26 @@
+package FunctionalProgramming_Exercise;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class FindTheSmallestElement {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        List<Integer> numbers = Arrays.stream(reader.readLine().split("\\s+"))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        Function<List<Integer>, Integer> minNumber = arr -> {
+            int min = arr.stream().mapToInt(Integer::intValue).min().getAsInt();
+            return arr.lastIndexOf(min);
+        };
+        
+        System.out.println(minNumber.apply(numbers));
+    }
+}
