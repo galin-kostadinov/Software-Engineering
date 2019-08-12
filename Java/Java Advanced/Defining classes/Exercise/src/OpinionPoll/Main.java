5477ac39a1cd88df1f586f499437d4e15c74391a
Galin Kostadinov <gk_1988@abv.bg>
2019-08-13 00:02:11 +0300

    Add OpinionPoll

---
+++ Java/Java Advanced/Defining classes/Exercise/src/OpinionPoll/Main.java
@@ -0,0 +1,33 @@
+package OpinionPoll;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int count = Integer.parseInt(reader.readLine());
+
+        List<Person> people = new ArrayList<>();
+        while (count-- > 0) {
+            String[] tokens = reader.readLine().split("\\s+");
+
+            String name = tokens[0];
+            int age = Integer.parseInt(tokens[1]);
+
+            Person person = new Person(name, age);
+
+            people.add(person);
+        }
+
+        people.stream()
+                .filter(person -> person.getAge() > 30)
+                .sorted(Comparator.comparing(Person::getName))
+                .forEach(person -> System.out.println(String.format("%s - %d", person.getName(), person.getAge())));
+    }
+}
