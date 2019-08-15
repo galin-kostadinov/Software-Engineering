53d91cb8e51d61f4ae09e72ed6afb97c2de4b756
Galin Kostadinov <gk_1988@abv.bg>
2019-08-15 17:59:19 +0300

    Add package GenericCountMethodStrings

---
+++ Java/Java Advanced/Generics/Exercise/src/GenericCountMethodStrings/Main.java
@@ -0,0 +1,39 @@
+package GenericCountMethodStrings;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        List<Box<String>> boxes = new ArrayList<>();
+
+        int n = Integer.parseInt(reader.readLine());
+
+        while (n-- > 0) {
+            Box<String> element = new Box<>(reader.readLine());
+            boxes.add(element);
+        }
+
+        String compareElement = reader.readLine();
+
+        int count = countGreaterElements(boxes, compareElement);
+
+        System.out.println(count);
+    }
+
+    private static <T extends Comparable<T>> int countGreaterElements(List<Box<T>> boxes, T compareElement) {
+        int count = 0;
+        for (Box<T> box : boxes) {
+            if (box.element.compareTo(compareElement) > 0){
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
