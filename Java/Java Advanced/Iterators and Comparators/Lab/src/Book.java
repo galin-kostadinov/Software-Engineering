f5c1df094faded8bdb6b8c968f04bdfe19f30e01
Galin Kostadinov <gk_1988@abv.bg>
2019-08-16 15:27:30 +0300

    Add project Iterators and Comparators/Lab

---
+++ Java/Java Advanced/Iterators and Comparators/Lab/src/Book.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Book {
+    private String title;
+    private int year;
+    private List<String> authors;
+
+    public Book(String title, int year, String... authors) {
+        this.title = title;
+        this.year = year;
+        this.authors = Arrays.asList(authors);
+    }
+
+    
+
+}
