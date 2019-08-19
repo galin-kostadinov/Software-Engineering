6613759c89359dd3fefcc6cc39d69d0b2d6f470f
Galin Kostadinov <gk_1988@abv.bg>
2019-08-19 22:57:40 +0300

    Add package: StudentSystem

---
+++ Java/Java OOP/WorkingWithAbstraction/Lab/src/StudentSystem/ShowCommand.java
@@ -0,0 +1,11 @@
+package StudentSystem;
+
+public class ShowCommand implements Command {
+    public void execute(StudentRepository repo, String[] args) {
+        var name = args[1];
+        if (repo.ContainsStudentByName(name)) {
+            var student = repo.findOneByName(name);
+            System.out.println(student);
+        }
+    }
+}
---
+++ Java/Java OOP/WorkingWithAbstraction/Lab/src/StudentSystem/StudentSystem.java
@@ -0,0 +1,25 @@
+package StudentSystem;
+
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class StudentSystem {
+    private StudentRepository repo;
+
+    public StudentSystem() {
+        this.repo = new StudentRepository();
+    }
+
+    @Deprecated
+    public StudentRepository getRepo() {
+        return this.repo;
+    }
+
+    public void ParseCommand(String[] args) {
+
+        Command command = CommandFactory.createCommand(args[0]);
+        command.execute(this.repo, args);
+    }
+}
