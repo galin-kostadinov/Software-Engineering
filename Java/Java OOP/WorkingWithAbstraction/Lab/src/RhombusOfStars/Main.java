308e9e4f1c5a47df2337d2399e00cd85fb76e523
Galin Kostadinov <gk_1988@abv.bg>
2019-08-19 19:50:19 +0300

    Add RhombusOfStars/Main.java

---
+++ Java/Java OOP/WorkingWithAbstraction/Lab/src/RhombusOfStars/Main.java
@@ -0,0 +1,36 @@
+package RhombusOfStars;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(reader.readLine());
+
+        for (int row = 1; row <= n; row++) {
+            printCurrentRow(n, row);
+        }
+
+        for (int row = n-1; row >= 1; row--) {
+            printCurrentRow(n, row);
+        }
+    }
+
+    private static void printCurrentRow(int width, int row) {
+        System.out.print(printSpace(" ", width - row));
+        System.out.print(printSpace("* ", row));
+        System.out.println();
+    }
+
+    private static String printSpace(String str, int count) {
+        StringBuilder sb = new StringBuilder();
+
+        while (count-- > 0) {
+            sb.append(str);
+        }
+        return sb.toString();
+    }
+}
