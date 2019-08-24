a7047935ccd4d2fd77045e383a8ae68a16ff41c0
Galin Kostadinov <gk_1988@abv.bg>
2019-08-24 23:26:43 +0300

    Add project Ferrari

---
+++ Java/Java OOP/InterfacesAndAbstraction/Lab/Ferrari/src/Main.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String name = reader.readLine();
+
+        Car Ferrari = new Ferrari(name);
+
+        System.out.println(Ferrari.toString());
+    }
+}
