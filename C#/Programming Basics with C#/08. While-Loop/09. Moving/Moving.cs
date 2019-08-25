0877cb443132793c6ffe21c4b8dd512407e78769
Galin Kostadinov <gk_1988@abv.bg>
2019-08-25 12:23:35 +0300

    Add tasks from package: 08. While-Loop

---
+++ C#/Programming Basics with C#/08. While-Loop/09. Moving/Moving.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Moving
+{
+    static void Main()
+    {
+        int freeSpaceWidth = int.Parse(Console.ReadLine());
+        int freeSpacеLenght = int.Parse(Console.ReadLine());
+        int freeSpacеHight = int.Parse(Console.ReadLine());
+        int freeSpase = freeSpaceWidth * freeSpacеLenght * freeSpacеHight;
+        int cartonVolume = 0;
+        var variable = "variable";
+        int numberOfCarton = 0;
+
+
+        while (freeSpase > cartonVolume && variable != "Done")
+        {
+            variable = Console.ReadLine();
+            if (variable != "Done")
+            {
+                numberOfCarton = int.Parse(variable);
+                cartonVolume = cartonVolume + numberOfCarton;
+            }
+        }
+        if (variable == "Done")
+        {
+            Console.WriteLine($"{freeSpase - cartonVolume} Cubic meters left.");
+        }
+        else
+        {
+            Console.WriteLine($"No more free space! You need {cartonVolume - freeSpase} Cubic meters more.");
+        }
+    }
+}
---
+++ C#/Programming Basics with C#/08. While-Loop/CWalking/WalkingII.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class WalkingII
+{
+    static void Main()
+    {
+
+        int sum = 0;
+        int stepTarget = 1000;
+        while (sum < stepTarget)
+        {
+            string a = Console.ReadLine();
+
+            if (a == "Going home")
+            {
+                sum = sum + int.Parse(Console.ReadLine());
+                break;
+            }
+            else
+            {
+                sum = sum + int.Parse(a);
+            }
+        }
+        if (sum >= stepTarget)
+        {
+            Console.WriteLine("Goal reached! Good job!");
+        }
+        else
+        {
+            Console.WriteLine($"{stepTarget - sum} more steps to reach goal.");
+        }
+    }
+}
