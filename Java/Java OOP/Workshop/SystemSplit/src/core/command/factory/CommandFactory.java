5bd83ad9f6330f12a46fb7bf313577afcebac78a
Galin Kostadinov <gk_1988@abv.bg>
2019-09-04 19:01:27 +0300

    Add package: reader; Update Engine.java, CommandFactory.java, Main.java

--- Java/Java OOP/Workshop/SystemSplit/src/core/command/factory/CommandFactory.java
+++ Java/Java OOP/Workshop/SystemSplit/src/core/command/factory/CommandFactory.java
@@ -5,15 +5,15 @@ import core.command.Command;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 
-public class CommandFactory {
+public final class CommandFactory {
     private static final String BASE_COMMAND_PACKAGE = "core.command.";
     private static final String COMMAND_SUFFIX = "Command";
 
-    public Command buildCommand(String name, Object... args) {
+    public static Command buildCommand(String name, Object... args) {
         try {
             Class commandClass = Class.forName(BASE_COMMAND_PACKAGE + name + COMMAND_SUFFIX);
             Constructor<Command> constuctor = commandClass.getDeclaredConstructors()[0];
-            return constuctor.newInstance(args);
+            return constuctor.newInstance(new Object[]{args});
         } catch (ClassNotFoundException
                 | IllegalAccessException
                 | InstantiationException
--- Java/Java OOP/Workshop/SystemSplit/src/core/engine/Engine.java
+++ Java/Java OOP/Workshop/SystemSplit/src/core/engine/Engine.java
@@ -1,7 +1,36 @@
 package core.engine;
 
+import core.command.Command;
+import core.command.factory.CommandFactory;
+import core.system.SystemSplit;
+import io.reader.Reader;
+
 public class Engine {
+    private static final String END_COMMAND = "System Split";
+
+    private SystemSplit systemSplit;
+    private Reader reader;
+
+    public Engine(SystemSplit systemSplit, Reader reader) {
+        this.systemSplit = systemSplit;
+        this.reader = reader;
+    }
+
     public void run() {
-        
+        String line = this.reader.readLine();
+        while (!END_COMMAND.equalsIgnoreCase(line)) {
+            String[] tokens = line.split("[(),\\s]+".trim());
+            String commandName = tokens[0];
+            Object[] params = new Object[tokens.length];
+            params[0] = this.systemSplit;
+            for (int i = 1; i < tokens.length; i++) {
+                params[i] = tokens[i];
+            }
+
+            Command command = CommandFactory.buildCommand(commandName, params);
+            command.execute();
+
+            line = this.reader.readLine();
+        }
     }
 }
