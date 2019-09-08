70796a08000e03a7969149fc2d8c980f8cdb0582
Galin Kostadinov <gk_1988@abv.bg>
2019-09-08 15:48:07 +0300

    Add project UnitTesting/Lab

---
+++ Java/Java OOP/UnitTesting/Lab/src/test/java/rpg_tests/AxeTests.java
@@ -0,0 +1,29 @@
+package rpg_tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+import rpg_lab.Axe;
+import rpg_lab.Dummy;
+
+public class AxeTests {
+
+    @Test
+    public void weaponAttacksLosesDurability() {
+        Axe axe = new Axe(10, 10);
+        Dummy dummy = new Dummy(20, 10);
+
+        axe.attack(dummy);
+
+        Assert.assertEquals(9, axe.getDurabilityPoints());
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void brokenWeaponCantAttack() {
+        Axe axe = new Axe(10, 1);
+        Dummy dummy = new Dummy(20, 10);
+
+        axe.attack(dummy);
+        axe.attack(dummy);
+        axe.attack(dummy);
+    }
+}
---
+++ Java/Java OOP/UnitTesting/Lab/src/test/java/rpg_tests/DummyTests.java
@@ -0,0 +1,5 @@
+package rpg_tests;
+
+public class DummyTests {
+
+}
