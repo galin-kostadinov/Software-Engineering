41195ed9d58625e18458b56685c113a475e7b015
Galin Kostadinov <gk_1988@abv.bg>
2019-09-09 23:01:45 +0300

    Add GeneratingCombinations.java

---
+++ Data Structures and Algorithms/Algorithms/01. Recursion, Sorting and Searching Algorithms/code/src/recursion/GeneratingCombinations.java
@@ -0,0 +1,39 @@
+package recursion;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class GeneratingCombinations {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] set = Arrays.stream(reader.readLine().split("\\s+"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        int cols = Integer.parseInt(reader.readLine());
+
+        int[] vector = new int[cols];
+        generator(set, vector, 0, -1);
+    }
+
+    private static void generator(int[] set, int[] vector, int index, int border) {
+        if (index == vector.length) {
+            print(vector);
+        } else {
+            for (int i = border + 1; i < set.length; i++) {
+                vector[index] = set[i];
+                generator(set, vector, index + 1, i);
+            }
+        }
+    }
+
+    private static void print(int[] vector) {
+        for (int value : vector) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+    }
+}
