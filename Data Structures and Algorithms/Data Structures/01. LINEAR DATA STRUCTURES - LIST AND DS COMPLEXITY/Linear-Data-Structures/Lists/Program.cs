f9a66c0682c19aa2ef85f490c5886bd16b32f212
Galin Kostadinov <gk_1988@abv.bg>
2019-09-12 23:19:04 +0300

    Add project: Linear-Data-Structures

---
+++ Data Structures and Algorithms/Data Structures/01. LINEAR DATA STRUCTURES - LIST AND DS COMPLEXITY/Linear-Data-Structures/Lists/Program.cs
@@ -0,0 +1,12 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        ArrayList<int> list = new ArrayList<int>();
+        list.Add(10);
+        list.Add(10);
+        list.Add(10);
+        list.Add(10);
+
+    }
+}
