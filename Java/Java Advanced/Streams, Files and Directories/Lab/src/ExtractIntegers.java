85ee4d76cc387b18d5281a15fa2d38a753a32241
Galin Kostadinov <gk_1988@abv.bg>
2019-10-02 23:37:18 +0300

    Update ExtractIntegers.java

--- Java/Java Advanced/Streams, Files and Directories/Lab/src/ExtractIntegers.java
+++ Java/Java Advanced/Streams, Files and Directories/Lab/src/ExtractIntegers.java
@@ -6,15 +6,16 @@ public class ExtractIntegers {
         String userDir = System.getProperty("user.dir");
 
         String pathIn = userDir + "\\res\\input.txt";
-        String pathOut = userDir + "\\res\\output.txt";
+        String pathOut = userDir + "\\res\\outputInt2.txt";
 
         try (Scanner sc = new Scanner(new FileReader(pathIn));
              PrintWriter writer = new PrintWriter(pathOut)) {
 
             while (sc.hasNext()) {
-                sc.next();
                 if (sc.hasNextInt()) {
                     writer.println(sc.nextInt());
+                } else {
+                    sc.next();
                 }
             }
         } catch (IOException e) {
