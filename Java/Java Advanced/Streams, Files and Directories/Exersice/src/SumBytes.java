9b94340049f548886461c269e9c7001512257ee3
Galin Kostadinov <gk_1988@abv.bg>
2019-10-04 21:59:16 +0300

    Update SumBytes.java

--- Java/Java Advanced/Streams, Files and Directories/Exersice/src/SumBytes.java
+++ Java/Java Advanced/Streams, Files and Directories/Exersice/src/SumBytes.java
@@ -3,12 +3,17 @@ import java.math.BigInteger;
 
 public class SumBytes {
     public static void main(String[] args) throws IOException {
-        String filePath = "E:\\ComputerProgramming\\Java\\03_JavaAdvanced\\Java Advanced\\Java Advanced - 201905\\10. Exercise - Streams, Files and Directories\\04. Java-Advanced-Files-and-Streams-Exercises-Resources\\input.txt";
+       // String filePath = "E:\\ComputerProgramming\\Java\\03_JavaAdvanced\\Java Advanced\\Java Advanced - 201905\\10. Exercise - Streams, Files and Directories\\04. Java-Advanced-Files-and-Streams-Exercises-Resources\\input.txt";
+
+        String userDir = System.getProperty("user.dir");
+        String filePath = userDir + "\\res\\input.txt";
+
         String outputPath = filePath.substring(0, filePath.lastIndexOf("\\") + 1) + "BigIntOut.txt";
 
         FileReader fileReader = new FileReader(filePath);
-
         BufferedReader reader = new BufferedReader(fileReader);
+
+       // BufferedReader reader1 = new BufferedReader(new InputStreamReader(new FileInputStream(filePath)));
 
         String line;
 
@@ -20,6 +25,7 @@ public class SumBytes {
             }
 
         }
+        reader.close();
 
         PrintWriter printWriter = new PrintWriter(outputPath);
 
