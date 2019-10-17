00b55a7517b90ff47de1ddac2f28ee25d1b82b0e
Galin Kostadinov <gk_1988@abv.bg>
2019-10-17 16:27:16 +0300

    Add Main and package repository

---
+++ Java/Java Advanced/Exams/Exam17Feb2019/src/repository/Person.java
@@ -0,0 +1,15 @@
+package repository;
+
+public class Person {
+    private String name;
+    private int age;
+    private String birthDate;
+
+    public Person(String name, int age, String birthDate) {
+        this.name = name;
+        this.age = age;
+        this.birthDate = birthDate;
+    }
+
+
+}
