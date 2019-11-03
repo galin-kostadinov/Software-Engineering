6be12bea964fd97bcde0baa6ddce8537033f3b79
Galin Kostadinov <gk_1988@abv.bg>
2019-11-03 12:28:56 +0200

    Add project Trie

---
+++ Data Structures and Algorithms/Data Structures/11. ROPE AND TRIE/Trie/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Trie trie = new Trie();
+        trie.insert("The", 1);
+        trie.insert("There", 2);
+        trie.insert("Trie", 3);
+        trie.insert("Tree", 4);
+        trie.insert("Rope", 5);
+        trie.insert("Their", 6);
+    }
+}
