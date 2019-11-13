f88bff4e890b3212ae9fe905d6e39289d7b14bb3
Galin Kostadinov <gk_1988@abv.bg>
2019-11-13 22:11:46 +0200

    Add Decompression.cpp

---
+++ C++/C++ Fundamentals Sept 2019/11. EXAM PREPARATION/SecondExamJan2018/src/Decompression.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+void makeDecompression(std::vector<std::pair<int, char>> &decompressChars) {
+    std::cin.sync_with_stdio(false);
+    std::cout.sync_with_stdio(false);
+
+    std::string text;
+    std::cin >> text;
+
+    std::string countAsString;
+
+    int size = (int) text.size();
+    for (int i = 0; i < size; ++i) {
+        char currChar = text[i];
+
+        if (isdigit(currChar)) {
+            countAsString.push_back(currChar);
+        } else {
+            if (countAsString.empty()) {
+                decompressChars.emplace_back(1, currChar);
+            } else {
+                int count = stoi(countAsString);
+                countAsString.clear();
+
+                decompressChars.emplace_back(count, currChar);
+            }
+        }
+    }
+}
+
+void print(std::vector<std::pair<int, char>> &decompressChars) {
+    std::cin.sync_with_stdio(false);
+    std::cout.sync_with_stdio(false);
+
+    char currChar;
+    int count;
+
+    int size = (int) decompressChars.size();
+
+    for (int j = 0; j < size; ++j) {
+        count = decompressChars[j].first;
+        currChar = decompressChars[j].second;
+
+        for (int i = 0; i < count; ++i) {
+            std::cout << currChar;
+        }
+    }
+
+    std::cout << std::endl;
+}
+
+int main() {
+    std::cin.sync_with_stdio(false);
+    std::cout.sync_with_stdio(false);
+
+    std::vector<std::pair<int, char>> decompressChars;
+
+    makeDecompression(decompressChars);
+    print(decompressChars);
+
+    return 0;
+}
