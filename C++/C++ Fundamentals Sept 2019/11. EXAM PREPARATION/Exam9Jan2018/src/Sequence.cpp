8de90faa24e12d500528cf8e8a42da94b1a6b126
Galin Kostadinov <gk_1988@abv.bg>
2019-11-22 16:22:53 +0200

    Add Sequence.cpp

---
+++ C++/C++ Fundamentals Sept 2019/11. EXAM PREPARATION/Exam9Jan2018/src/Sequence.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <map>
+#include <vector>
+
+int main() {
+    int count;
+    std::cin >> count;
+
+    int max = 0;
+    int secondMax = 0;
+    int currentNumber = 0;
+    int lastNumber = 0;
+    for (int i = 0; i < count; ++i) {
+        std::cin >> currentNumber;
+
+        if (i == 0) {
+            secondMax++;
+        } else {
+            if (currentNumber > lastNumber) {
+                secondMax++;
+            } else {
+                if (max < secondMax) {
+                    max = secondMax;
+                }
+                secondMax = 1;
+            }
+        }
+
+        lastNumber = currentNumber;
+    }
+
+    if (max >= secondMax) {
+        std::cout << max << std::endl;
+    } else {
+        std::cout << secondMax << std::endl;
+    }
+
+    return 0;
+}
