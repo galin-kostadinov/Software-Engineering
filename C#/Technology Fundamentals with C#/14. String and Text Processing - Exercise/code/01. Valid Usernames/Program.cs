973383dba5b2b0af0843e5b498a722966162c4d9
Galin Kostadinov <gk_1988@abv.bg>
2019-11-25 00:07:06 +0200

    Add 14. String and Text Processing - Exercise/code/01. Valid Usernames

---
+++ C#/Technology Fundamentals with C#/14. String and Text Processing - Exercise/code/01. Valid Usernames/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01._Valid_Usernames
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] userNames = Console.ReadLine().Split(", ");
+            List<string> legalUserNames = new List<string>();
+
+            foreach (var userName in userNames)
+            {
+                if (userName.Length<3 || userName.Length>16)
+                {
+                    continue;
+                }
+                bool isNotLetter = false;
+                foreach (var symbol in userName)
+                {
+                    if (!(symbol=='-' || symbol == '_' || char.IsLetter(symbol) || char.IsDigit(symbol)))
+                    {
+                        isNotLetter = true;
+                        break;
+                    }
+                }
+                if (isNotLetter)
+                {
+                    continue;
+                }
+
+                legalUserNames.Add(userName);
+            }
+            foreach (var userName in legalUserNames)
+            {
+                Console.WriteLine(userName);
+            }
+        }
+    }
+}
