42635482facd96727b207bf43c954a645a15e97e
Galin Kostadinov <gk_1988@abv.bg>
2019-11-25 22:29:37 +0200

    Add 02. Character Multiplier

---
+++ C#/Technology Fundamentals with C#/14. String and Text Processing - Exercise/code/02. Character Multiplier/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CharacterMultipl
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] input = Console.ReadLine().Split();
+
+            string firstString = input[0];
+            string secondString = input[1];
+
+            int totalSum = 0;
+            int minLenght = Math.Min(firstString.Length, secondString.Length);
+            int maxLenght = Math.Max(firstString.Length, secondString.Length);
+
+            for (int i = 0; i < minLenght; i++)
+            {
+                totalSum += firstString[i] * secondString[i];
+            }
+            for (int i = minLenght; i < maxLenght; i++)
+            {
+                if (firstString.Length > secondString.Length)
+                {
+                    totalSum += firstString[i];
+                }
+                else
+                {
+                    totalSum += secondString[i];
+                }
+            }
+
+            Console.WriteLine(totalSum);
+        }
+    }
+}
