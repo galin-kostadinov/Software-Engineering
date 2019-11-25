4483e87c4acb954e2c8cf85a52b1d9a798e1926d
Galin Kostadinov <gk_1988@abv.bg>
2019-11-25 22:30:18 +0200

    Add 03. Extract File

---
+++ C#/Technology Fundamentals with C#/14. String and Text Processing - Exercise/code/03. Extract File/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace Extract_File
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] path = Console.ReadLine().Split('\\');
+            string[] fileNameAndExtention = path[path.Length - 1].Split('.');
+            Console.WriteLine($"File name: {fileNameAndExtention[0]}");
+            Console.WriteLine($"File extension: {fileNameAndExtention[1]}");
+        }
+    }
+}
