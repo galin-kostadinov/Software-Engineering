3e7e21829cf8b914eb9cc704fe5c1e3a36a1a77d
Galin Kostadinov <gk_1988@abv.bg>
2019-11-26 11:41:24 +0200

    Add 04. Caesar Cipher

---
+++ C#/Technology Fundamentals with C#/14. String and Text Processing - Exercise/code/04. Caesar Cipher/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+namespace Caesar_Cipher
+{
+    class Program
+    {
+        static void Main()
+        {
+            string text = Console.ReadLine();
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char replaceChar = (char)(text[i] + 3);
+                sb.Append(replaceChar);
+            }
+
+            Console.WriteLine(sb);
+        }
+
+    }
+}
