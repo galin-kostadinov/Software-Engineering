41690b98699d6498dff8fd0fee836f181f61ae55
Galin Kostadinov <gk_1988@abv.bg>
2019-11-26 11:42:26 +0200

    Add 06. ReplRepeatChars

---
+++ C#/Technology Fundamentals with C#/14. String and Text Processing - Exercise/code/06. ReplRepeatChars/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace ReplRepeatChars
+{
+    class Program
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                if (input[i] != input[i + 1])
+                {
+                    sb.Append(input[i]);
+                }
+                if (i == input.Length - 2)
+                {
+                    sb.Append(input[i + 1]);
+                }
+            }
+
+            Console.WriteLine(sb);
+        }
+    }
+}
