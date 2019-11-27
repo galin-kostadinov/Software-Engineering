e9916861e8036cef34e843cddb6cc6fcaf855064
Galin Kostadinov <gk_1988@abv.bg>
2019-11-27 22:41:56 +0200

    Add 02. Match Phone Number

---
+++ C#/Technology Fundamentals with C#/15. Regular Expressions/RegularExpr-Regex-Lab/02. Match Phone Number/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+public class Example
+{
+    public static void Main()
+    {
+        string pattern = @"\+(359)([ -]+)(2)\2[0-9]{3}\2[0-9]{4}\b";
+        string input = Console.ReadLine();
+        RegexOptions options = RegexOptions.Multiline;
+        List<string> matchedDates = new List<string>();
+
+        foreach (Match m in Regex.Matches(input, pattern, options))
+        {
+            matchedDates.Add(m.Value);
+        }
+        Console.WriteLine(string.Join(", ", matchedDates));
+    }
+}
