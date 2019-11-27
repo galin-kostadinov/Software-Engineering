a9dc429922f8024f0718f6701f60dcfcaf317bb9
Galin Kostadinov <gk_1988@abv.bg>
2019-11-27 22:38:48 +0200

    Add 08. LettersChangeNum

---
+++ C#/Technology Fundamentals with C#/14. String and Text Processing - Exercise/code/08. LettersChangeNum/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+namespace LettersChangeNum
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] input = Console.ReadLine().Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+            double totalSum = 0.0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                string currentInput = input[i];
+                int numberLength = currentInput.Length - 2;
+                double number = double.Parse(currentInput.Substring(1, numberLength));
+
+                char firstLetter = currentInput[0];
+                char lastLetter = currentInput[currentInput.Length - 1];
+
+                double result = 0;
+
+                if (char.IsUpper(firstLetter))
+                {
+                    int letterPossition = firstLetter - 'A' + 1;
+                    result = number / letterPossition;
+                }
+                else
+                {
+                    int letterPossition = firstLetter - 'a' + 1;
+                    result = number * letterPossition;
+                }
+
+                if (char.IsUpper(lastLetter))
+                {
+                    int letterPossition = lastLetter - 'A' + 1;
+                    totalSum += result - letterPossition;
+                }
+                else
+                {
+                    int letterPossition = lastLetter - 'a' + 1;
+                    totalSum += result + letterPossition;
+                }
+            }
+
+            Console.WriteLine($"{totalSum:F2}");
+        }
+    }
+}
