f0c7206c03bf909ca16cb2f3792e1ef9499db26e
Galin Kostadinov <gk_1988@abv.bg>
2019-11-28 21:03:16 +0200

    Add 03. Match Dates

---
+++ C#/Technology Fundamentals with C#/15. Regular Expressions/RegularExpr-Regex-Lab/03. Match Dates/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class Example
+{
+    public static void Main()
+    {
+        string pattern = @"\b(\d{2})([-.\/])([A-Z][a-z]{2})\2(\d{4})\b";
+        string input = Console.ReadLine();
+        RegexOptions options = RegexOptions.Multiline;
+
+        foreach (Match m in Regex.Matches(input, pattern, options))
+        {
+            string[] date = m.Value.Split(new char[] { '.', '/', '-' });
+
+            Console.WriteLine($"Day: {date[0]}, Month: {date[1]}, Year: {date[2]}");
+        }
+    }
+}
