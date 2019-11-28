2fa18645355297da0dd205263b43d1e10fe8bcbc
Galin Kostadinov <gk_1988@abv.bg>
2019-11-28 21:04:08 +0200

    Add 04. MatchNumbers

---
+++ C#/Technology Fundamentals with C#/15. Regular Expressions/RegularExpr-Regex-Lab/04. MatchNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class Example
+{
+    public static void Main()
+    {
+        string pattern = @"(^|(?<=\s))-?\d+(\.\d+)?($|(?=\s))";
+        string input = Console.ReadLine();
+        RegexOptions options = RegexOptions.Multiline;
+
+        foreach (Match m in Regex.Matches(input, pattern, options))
+        {
+            Console.Write(m.Value + " ");
+        }
+        Console.WriteLine();
+    }
+}
