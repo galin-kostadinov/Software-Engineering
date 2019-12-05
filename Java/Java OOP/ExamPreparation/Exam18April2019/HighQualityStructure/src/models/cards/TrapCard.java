9f0a96669f70f392c1281db4717e3090de023bc4
Galin Kostadinov <gk_1988@abv.bg>
2019-12-05 13:38:39 +0200

    Add TrapCard.java

---
+++ Java/Java OOP/ExamPreparation/Exam18April2019/HighQualityStructure/src/models/cards/TrapCard.java
@@ -0,0 +1,11 @@
+package models.cards;
+
+public class TrapCard extends BaseCard {
+    private final static int DEFAULT_DAMAGE_POINTS = 120;
+    private final static int DEFAULT_HEALTH_POINTS = 5;
+
+
+    protected TrapCard(String name) {
+        super(name, DEFAULT_DAMAGE_POINTS, DEFAULT_HEALTH_POINTS);
+    }
+}
