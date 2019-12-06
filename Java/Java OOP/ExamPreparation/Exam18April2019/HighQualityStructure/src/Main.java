587a9a991fd47c00c786269e69ca8e4f03fde58d
Galin Kostadinov <gk_1988@abv.bg>
2019-12-06 12:18:23 +0200

    Add Main.java

--- Java/Java OOP/ExamPreparation/Exam18April2019/HighQualityStructure/src/Main.java
+++ Java/Java OOP/ExamPreparation/Exam18April2019/HighQualityStructure/src/Main.java
@@ -1,6 +1,34 @@
+
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello World!");
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+//
+//        ManagerController managerController = new ManagerControllerImpl();
+//
+//        String line;
+//
+//        while (!"Exit".equals(line = reader.readLine())) {
+//            String[] tokens = line.split("\\s++");
+//
+//            switch (tokens[0]) {
+//                case "AddPlayer":
+//                    System.out.println(managerController.addPlayer(tokens[1], tokens[2]));
+//                    break;
+//                case "AddCard":
+//                    System.out.println(managerController.addCard(tokens[1], tokens[2]));
+//                    break;
+//                case "AddPlayerCard":
+//                    System.out.println(managerController.addPlayerCard(tokens[1], tokens[2]));
+//                    break;
+//                case "Fight":
+//                    System.out.println(managerController.fight(tokens[1], tokens[2]));
+//                    break;
+//                case "Report":
+//                    System.out.println(managerController.report());
+//                    break;
+//                default:
+//                    break;
+//            }
+//        }
     }
 }
