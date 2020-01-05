1412b84bb0d37c0c49bd051186c4aacd314800b8
Galin Kostadinov <gk_1988@abv.bg>
2020-01-05 23:46:02 +0200

    Add package 01_Census: CensusMain.cpp, City.h, CityUtils.h

---
+++ C++/C++ Advanced Nov 2019/12. Exam Preparation/DemoExam/01_Census/CityUtils.h
@@ -0,0 +1,31 @@
+#ifndef CITYUTILS_H
+#define CITYUTILS_H
+
+#include <string>
+#include <vector>
+#include <map>
+#include "City.h"
+
+#include <memory>
+
+City *initCity(std::string name, size_t population) {
+    return new City(name, population);
+}
+
+std::map<size_t, std::shared_ptr<City>> groupByPopulation(std::vector<const City *> cities, size_t &totalPopulation) {
+    std::map<size_t, std::shared_ptr<City>> result;
+    totalPopulation = 0;
+
+    size_t size = cities.size();
+    for (size_t i = 0; i < size; ++i) {
+        size_t popiulation = cities[i]->getPopulation();
+
+        result[popiulation] = std::make_shared<City>(cities[i]->getName(), popiulation);
+
+        totalPopulation += popiulation;
+    }
+
+    return result;
+}
+
+#endif //!CITYUTILS_H
