fa48bd54f0484f3942aa855a64190b7269e00af0
Galin Kostadinov <gk_1988@abv.bg>
2020-01-17 22:26:35 +0200

    Add task 02_Censorship: Article13Filter.cpp, Article13Filter.h, CensorshipMain.cpp

---
+++ C++/C++ Advanced Nov 2019/06. Code Organization and Templates/codecpp/02_Censorship/Article13Filter.cpp
@@ -0,0 +1,20 @@
+#include "Article13Filter.h"
+
+Article13Filter::Article13Filter(std::set<std::string> copyrighted) : copyrighted(copyrighted) {}
+
+bool Article13Filter::blockIfCopyrighted(std::string s) {
+    if (isCopyrighted(s)) {
+        this->blocked.emplace_back(s);
+        return true;
+    }
+
+    return false;
+}
+
+bool Article13Filter::isCopyrighted(std::string s) {
+    return this->copyrighted.find(s) != this->copyrighted.end();
+}
+
+std::vector<std::string> Article13Filter::getBlocked() {
+    return this->blocked;
+}
