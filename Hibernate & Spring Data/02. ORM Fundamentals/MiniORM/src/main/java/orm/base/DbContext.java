234bba61d34bb350d9d473feedd232674437b188
Galin Kostadinov <gk_1988@abv.bg>
2020-01-29 20:38:22 +0200

    Add DbContext.java

---
+++ Hibernate & Spring Data/02. ORM Fundamentals/MiniORM/src/main/java/orm/base/DbContext.java
@@ -0,0 +1,18 @@
+package orm.base;
+
+import java.sql.SQLException;
+
+public interface DbContext<E> {
+   //insert + update
+    boolean persist(E entity) throws IllegalAccessException, SQLException;
+
+    Iterable<E> find() throws SQLException, IllegalAccessException, InstantiationException;
+
+    Iterable<E> find(String where) throws SQLException, IllegalAccessException, InstantiationException;
+
+    E findFirst() throws IllegalAccessException, SQLException, InstantiationException;
+
+    E findFirst(String where) throws IllegalAccessException, SQLException, InstantiationException;
+
+    E findById(long id) throws IllegalAccessException, SQLException, InstantiationException;
+}
