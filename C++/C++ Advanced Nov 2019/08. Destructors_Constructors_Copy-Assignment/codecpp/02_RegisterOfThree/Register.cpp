102a24537e80ac94f3645aff0c9af76dd9ad3324
Galin Kostadinov <gk_1988@abv.bg>
2020-01-30 23:48:07 +0200

    Add task 02_RegisterOfThree: Company.h, Register.cpp, Register.h, RegisterMain.cpp

---
+++ C++/C++ Advanced Nov 2019/08. Destructors_Constructors_Copy-Assignment/codecpp/02_RegisterOfThree/Register.cpp
@@ -0,0 +1,52 @@
+#include <algorithm>
+
+#include "Register.h"
+
+Register::Register(size_t numCompanies) : numAdded(0), companiesArray(new Company[numCompanies]) {}
+
+Register &Register::operator=(const Register &other) {
+    if (this != &other) {
+        if (this->companiesArray != nullptr) {
+            delete[] this->companiesArray;
+        }
+
+        this->numAdded = other.numAdded;
+        this->companiesArray = new Company[other.numAdded];
+
+        int arrSize = (size_t) other.numAdded;
+        for (int i = 0; i < arrSize; ++i) {
+            this->companiesArray[i] = other.companiesArray[i];
+        }
+    }
+
+    return *this;
+}
+
+Register::Register(const Register &other) {
+    this->numAdded = other.numAdded;
+
+    int arrSize = (size_t) other.numAdded;
+    for (int i = 0; i < arrSize; ++i) {
+        this->companiesArray[i] = other.companiesArray[i];
+    }
+}
+
+void Register::add(const Company &c) {
+    this->companiesArray[this->numAdded] = c;
+    this->numAdded++;
+}
+
+Company Register::get(int companyId) const {
+    for (Company *it = companiesArray; it != companiesArray + numAdded; it++) {
+        if ((*it).getId() == companyId) {
+            return *it;
+        }
+    }
+
+    return Company();
+}
+
+Register::~Register() {
+    delete[] this->companiesArray;
+}
+
