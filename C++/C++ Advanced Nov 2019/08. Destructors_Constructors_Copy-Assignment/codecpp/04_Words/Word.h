2a0cf182d713f80fb9c09d4ad11b7fd89e1759c2
Galin Kostadinov <gk_1988@abv.bg>
2020-02-02 20:36:28 +0200

    Add task 04_Words: Word.h, WordsMain.cpp

---
+++ C++/C++ Advanced Nov 2019/08. Destructors_Constructors_Copy-Assignment/codecpp/04_Words/Word.h
@@ -0,0 +1,42 @@
+#ifndef WORD_H
+#define WORD_H
+
+#include <unordered_map>
+#include <string>
+std::unordered_map<std::string, int> words{};
+
+    class Word {
+private:
+    std::string word;
+    bool removeFromWordsOnDestruct;
+public:
+    explicit Word(std::string word) : word(std::move(word)), removeFromWordsOnDestruct(false) {
+        ++words[this->word];
+    }
+
+    Word(const Word &other) : word(other.word), removeFromWordsOnDestruct(true) {z}
+
+    ~Word() {
+        if (this->removeFromWordsOnDestruct) {
+            words.erase(this->word);
+        }
+    }
+
+    std::string getWord() const {
+        return this->word;
+    }
+
+    int getCount() const {
+        return words[this->word];
+    }
+
+    Word &operator=(const Word &other) = delete;
+
+    bool operator<(const Word &other) const {
+        return this->word < other.word;
+    }
+
+    friend std::ostream &operator<<(std::ostream &out, const Word &w);
+};
+
+#endif //!WORD_H
