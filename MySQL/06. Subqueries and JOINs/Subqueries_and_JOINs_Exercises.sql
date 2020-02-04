aa6e5c2c05f2e931db005aae8b9bc519b74e52d1
Galin Kostadinov <gk_1988@abv.bg>
2020-02-04 23:31:42 +0200

    Update Subqueries_and_JOINs_Exercises.sql

--- MySQL/06. Subqueries and JOINs/Subqueries_and_JOINs_Exercises.sql
+++ MySQL/06. Subqueries and JOINs/Subqueries_and_JOINs_Exercises.sql
@@ -183,20 +183,18 @@ ORDER BY c.`continent_code`, c.`continent_code`;
 
 #16. Countries without any Mountains
 
-SELECT c.`country_name`, MAX(p.`elevation`) AS highest_peak_elevation, MAX(r.`length`) AS longest_river_length
-FROM `countries` AS c
-LEFT JOIN mountains_countries AS mc
-ON mc.`country_code` = c.`country_code`
-LEFT JOIN `peaks` AS p
-ON mc.`mountain_id` = p.`mountain_id`
-LEFT JOIN `countries_rivers` AS cr
-ON c.`country_code` = cr.`country_code`
-LEFT JOIN `rivers` AS r
-ON cr.`river_id` = r.`id`
-GROUP BY c.`country_name`
-ORDER BY highest_peak_elevation DESC, longest_river_length DESC, c.`country_name` ASC
-LIMIT 5;
-
+SELECT 
+    COUNT(*) as 'country_count'
+FROM
+    (SELECT 
+        mc.country_code AS 'mc_country_code'
+    FROM
+        `mountains_countries` AS mc
+    GROUP BY mc.country_code) AS d
+        RIGHT JOIN
+    `countries` AS c ON c.country_code = d.mc_country_code
+WHERE
+    d.mc_country_code IS NULL;
 
 #17. Highest Peak and Longest River by Country
 
