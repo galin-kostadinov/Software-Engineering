e456a50f7c014a06f69c401f12decf440c1663c7
Galin Kostadinov <gk_1988@abv.bg>
2020-02-12 21:25:18 +0200

    Add MergeSort.java

---
+++ Data Structures and Algorithms/Algorithms/01. Recursion, Sorting and Searching Algorithms/Algorithms-Sorting-Lab/src/MergeSort.java
@@ -0,0 +1,51 @@
+public class MergeSort {
+    static void merge(int arr[], int start, int mid, int end) {
+        if (mid < 0 || mid + 1 >= arr.length || arr[mid] <= arr[mid + 1]) {
+            return;
+        }
+
+        int temp[] = new int[end - start + 1];
+        int i = start;
+        int j = mid + 1;
+        int k = 0;
+
+        while (i <= mid && j <= end) {
+            if (arr[i] <= arr[j]) {
+                temp[k++] = arr[i++];
+            } else {
+                temp[k++] = arr[j++];
+            }
+        }
+
+        while (i <= mid) {
+            temp[k++] = arr[i++];
+        }
+
+        while (j <= end) {
+            temp[k++] = arr[j++];
+        }
+
+        for (i = start; i <= end; i++) {
+            arr[i] = temp[i - start];
+        }
+    }
+
+    static void mergeSort(int arr[], int start, int end) {
+        if (start < end) {
+            int mid = (start + end) / 2;
+            mergeSort(arr, start, mid);
+            mergeSort(arr, mid + 1, end);
+            merge(arr, start, mid, end);
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {5, 2, 6, 3, 7};
+        mergeSort(arr, 0, 4);
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
