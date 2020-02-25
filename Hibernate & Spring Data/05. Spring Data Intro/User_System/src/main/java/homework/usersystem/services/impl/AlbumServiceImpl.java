d89e631953c45c0aee7646a656f4cc2b34d27b52
Galin Kostadinov <gk_1988@abv.bg>
2020-02-25 20:33:17 +0200

    Update AlbumServiceImpl.java

---
+++ Hibernate & Spring Data/05. Spring Data Intro/User_System/src/main/java/homework/usersystem/services/impl/AlbumServiceImpl.java
@@ -0,0 +1,11 @@
+package homework.usersystem.services.impl;
+
+import homework.usersystem.services.AlbumService;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+public class AlbumServiceImpl implements AlbumService {
+}
