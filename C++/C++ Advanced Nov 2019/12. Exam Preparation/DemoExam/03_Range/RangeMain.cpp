8aac3caee49abfcca38de5e2d6f3005dee48c787
Galin Kostadinov <gk_1988@abv.bg>
2020-02-25 20:35:22 +0200

    Update RangeMain.cpp

--- C++/C++ Advanced Nov 2019/12. Exam Preparation/DemoExam/03_Range/RangeMain.cpp
+++ C++/C++ Advanced Nov 2019/12. Exam Preparation/DemoExam/03_Range/RangeMain.cpp
@@ -6,44 +6,44 @@
 #include "Range.h"
 
 Range readRange(std::string line) {
-  Range r;
-  std::istringstream in(line);
+    Range r;
+    std::istringstream in(line);
 
-  int number;
-  while (in >> number) {
-    r.add(number);
-  }
+    int number;
+    while (in >> number) {
+        r.add(number);
+    }
 
-  return r;
+    return r;
 }
 
 int main() {
-  std::ios::sync_with_stdio(false);
+    std::ios::sync_with_stdio(false);
 
-  std::string line;
-  std::getline(std::cin, line);
-
-  std::vector<Range> allRanges;
-
-  while (line != "end") {
-    Range r = readRange(line);
-    allRanges.push_back(r);
+    std::string line;
     std::getline(std::cin, line);
-  }
 
-  int query;
-  std::cin >> query;
+    std::vector<Range> allRanges;
 
-  Range maxOccurrencesRange(allRanges[0]);
-  for (size_t i = 1; i < allRanges.size(); i++) {
-    if (allRanges[i].getCount(query) > maxOccurrencesRange.getCount(query)) {
-      maxOccurrencesRange = allRanges[i];
+    while (line != "end") {
+        Range r = readRange(line);
+        allRanges.push_back(r);
+        std::getline(std::cin, line);
     }
-  }
 
-  for (T value : maxOccurrencesRange) {
-    std::cout << value << " ";
-  }
+    int query;
+    std::cin >> query;
 
-  return 0;
+    Range maxOccurrencesRange(allRanges[0]);
+    for (size_t i = 1; i < allRanges.size(); i++) {
+        if (allRanges[i].getCount(query) > maxOccurrencesRange.getCount(query)) {
+            maxOccurrencesRange = allRanges[i];
+        }
+    }
+
+    for (T value : maxOccurrencesRange) {
+        std::cout << value << " ";
+    }
+
+    return 0;
 }
