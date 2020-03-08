cf1a4c8a2ae8012a8b6cd46f168616793c82edff
Galin Kostadinov <gk_1988@abv.bg>
2020-03-09 00:46:24 +0200

    Add AddItem.java

---
+++ Hibernate & Spring Data/07. Spring Data Auto Mapping Objects/Game_Store/src/main/java/homework/gamestore/commands/shop/AddItem.java
@@ -0,0 +1,18 @@
+package homework.gamestore.commands.shop;
+
+import homework.gamestore.commands.Command;
+import homework.gamestore.domain.dtos.GameTitleViewDto;
+import homework.gamestore.services.GameService;
+import homework.gamestore.services.UserService;
+
+public class AddItem extends Command {
+    public AddItem(UserService userService, GameService gameService) {
+        super(userService, gameService);
+    }
+
+    @Override
+    public String execute(String... args) {
+        GameTitleViewDto gameTitleViewDto = new GameTitleViewDto(args[0]);
+        return this.getUserService().addGameToUserCart(gameTitleViewDto);
+    }
+}
