76df42d2c8d6896054dfabe055138112ad143e2b
Galin Kostadinov <gk_1988@abv.bg>
2020-03-09 00:49:36 +0200

    Add GameStoreController.java

---
+++ Hibernate & Spring Data/07. Spring Data Auto Mapping Objects/Game_Store/src/main/java/homework/gamestore/controllers/GameStoreController.java
@@ -0,0 +1,114 @@
+package homework.gamestore.controllers;
+
+import homework.gamestore.commands.*;
+import homework.gamestore.commands.game.AddGame;
+import homework.gamestore.commands.game.DeleteGame;
+import homework.gamestore.commands.game.EditGame;
+import homework.gamestore.commands.shop.AddItem;
+import homework.gamestore.commands.shop.BuyItem;
+import homework.gamestore.commands.shop.RemoveItem;
+import homework.gamestore.commands.user.Login;
+import homework.gamestore.commands.user.Logout;
+import homework.gamestore.commands.user.Register;
+import homework.gamestore.commands.view.AllGames;
+import homework.gamestore.commands.view.DetailGame;
+import homework.gamestore.commands.view.OwnedGames;
+import homework.gamestore.services.GameService;
+import homework.gamestore.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Controller;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+import static homework.gamestore.constants.CommandConstants.*;
+
+@Controller
+public class GameStoreController implements CommandLineRunner {
+    private final UserService userService;
+    private final GameService gameService;
+
+    @Autowired
+    public GameStoreController(UserService userService, GameService gameService) {
+        this.userService = userService;
+        this.gameService = gameService;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            System.out.println("-----------------------------------------------------------------------------------");
+            System.out.println("Commands format:");
+            System.out.println("RegisterUser|<email>|<password>|<confirmPassword>|<fullName>");
+            System.out.println("LoginUser|<email>|<password>");
+            System.out.println("Logout");
+            System.out.println("AddGame|<title>|<price>|<size>|<trailer>|<thubnailURL>|<description>|<releaseDate>");
+            System.out.println("EditGame|<id>|<values>");
+            System.out.println("AllGames");
+            System.out.println("DetailsGame|<gameTitle>");
+            System.out.println("OwnedGames");
+            System.out.println("AddItem|<gameTitle>");
+            System.out.println("RemoveItem|<gameTitle>");
+            System.out.println("BuyItem");
+            System.out.println("-----------------------------------------------------------------------------------");
+
+            System.out.println("Please, Enter the command:");
+
+            String[] params = br.readLine().split("\\|");
+
+            Command command = null;
+            switch (params[0]) {
+                case REGISTER_USER:
+                    command = new Register(userService, gameService);
+                    break;
+                case LOGIN_USER:
+                    command = new Login(userService, gameService);
+                    break;
+                case LOGOUT_USER:
+                    command = new Logout(userService, gameService);
+                    break;
+                case ADD_GAME:
+                    command = new AddGame(userService, gameService);
+                    break;
+                case EDIT_GAME:
+                    command = new EditGame(userService, gameService);
+                    break;
+                case DELETE_GAME:
+                    command = new DeleteGame(userService, gameService);
+                    break;
+                case ALL_GAME:
+                    command = new AllGames(userService, gameService);
+                    break;
+                case DETAIL_GAME:
+                    command = new DetailGame(userService, gameService);
+                    break;
+                case OWNED_GAME:
+                    command = new OwnedGames(userService, gameService);
+                    break;
+                case ADD_ITEM:
+                    command = new AddItem(userService, gameService);
+                    break;
+                case REMOVE_ITEM:
+                    command = new RemoveItem(userService, gameService);
+                    break;
+                case BUY_ITEM:
+                    command = new BuyItem(userService, gameService);
+                    break;
+                default:
+                    break;
+            }
+
+            if (command == null) {
+                continue;
+            }
+
+            params = Arrays.copyOfRange(params, 1, params.length);
+
+            System.out.println(command.execute(params));
+        }
+    }
+}
