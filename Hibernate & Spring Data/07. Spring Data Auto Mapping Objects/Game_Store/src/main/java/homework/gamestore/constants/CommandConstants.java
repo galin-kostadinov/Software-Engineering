79b40d6cbffbbaf59713beb0e3c387f6d1bbef76
Galin Kostadinov <gk_1988@abv.bg>
2020-03-09 00:48:44 +0200

    Add CommandConstants.java

---
+++ Hibernate & Spring Data/07. Spring Data Auto Mapping Objects/Game_Store/src/main/java/homework/gamestore/constants/CommandConstants.java
@@ -0,0 +1,19 @@
+package homework.gamestore.constants;
+
+public class CommandConstants {
+    public static final String REGISTER_USER = "RegisterUser";
+    public static final String LOGIN_USER = "LoginUser";
+    public static final String LOGOUT_USER = "Logout";
+
+    public static final String ADD_GAME = "AddGame";
+    public static final String EDIT_GAME = "EditGame";
+    public static final String DELETE_GAME = "DeleteGame";
+
+    public static final String ALL_GAME = "AllGames";
+    public static final String DETAIL_GAME = "DetailGame";
+    public static final String OWNED_GAME = "OwnedGames";
+
+    public static final String ADD_ITEM = "AddItem";
+    public static final String REMOVE_ITEM = "RemoveItem";
+    public static final String BUY_ITEM = "BuyItem";
+}
