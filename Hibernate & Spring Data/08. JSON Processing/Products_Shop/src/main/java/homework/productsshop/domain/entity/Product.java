d4272e89573e56c37be492fd1f76e9061eb2c3ee
Galin Kostadinov <gk_1988@abv.bg>
2020-03-12 10:37:34 +0200

    Add Product.java

---
+++ Hibernate & Spring Data/08. JSON Processing/Products_Shop/src/main/java/homework/productsshop/domain/entity/Product.java
@@ -0,0 +1,71 @@
+package homework.productsshop.domain.entity;
+
+import javax.persistence.*;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Size;
+import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "products")
+public class Product extends BaseEntity {
+    private String name;
+    private BigDecimal price;
+    private User seller;
+    private User buyer;
+    private Set<Category> categories;
+
+    public Product() {
+        this.categories = new HashSet<>();
+    }
+
+    @Size(min = 3)
+    @Column(name = "name", nullable = false)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Column(name = "price", nullable = false)
+    @DecimalMin(value = "0", message = " Price cannot be negative.")
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    @ManyToOne(optional = false, targetEntity = User.class, cascade = CascadeType.ALL)
+    @JoinColumn(name = "seller_id", referencedColumnName = "id")
+    public User getSeller() {
+        return seller;
+    }
+
+    public void setSeller(User seller) {
+        this.seller = seller;
+    }
+
+    @ManyToOne(optional = true, targetEntity = User.class, fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(name = "buyer_id")
+    public User getBuyer() {
+        return buyer;
+    }
+
+    public void setBuyer(User buyer) {
+        this.buyer = buyer;
+    }
+
+    @ManyToMany(mappedBy = "products", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
+    }
+}
