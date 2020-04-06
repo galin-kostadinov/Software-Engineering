04f9f31730361de04d0f310058482283ac57a2b3
galin-kostadinov <gk_1988@abv.bg>
2020-04-06 13:45:20 +0300

    add all logic

--- JavaWeb/Basic/02. Introduction to Java EE/WestCompassDealerShop/src/main/java/app/web/servlets/HomeServlet.java
+++ JavaWeb/Basic/02. Introduction to Java EE/WestCompassDealerShop/src/main/java/app/web/servlets/HomeServlet.java
@@ -1,4 +1,28 @@
 package app.web.servlets;
 
-public class HomeServlet {
+import app.util.FileUtil;
+
+import javax.inject.Inject;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/")
+public class HomeServlet extends HttpServlet {
+    private static String HOME_FILE_VIEW_PATH ="F:\\ComputerProgramming\\GitHubRepoSoftUniTasks\\Software-Engineering\\JavaWeb\\Basic\\02. Introduction to Java EE\\WestCompassDealerShop\\src\\main\\webapp\\views\\home.html";
+    private final FileUtil fileUtil;
+
+    @Inject
+    public HomeServlet(FileUtil fileUtil) {
+        this.fileUtil = fileUtil;
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String html = fileUtil.readFile(HOME_FILE_VIEW_PATH);
+        resp.getWriter().println(html);
+    }
 }
