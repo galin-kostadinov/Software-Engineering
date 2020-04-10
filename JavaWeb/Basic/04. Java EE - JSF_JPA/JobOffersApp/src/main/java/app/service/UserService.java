b68bd13bfd5e88b05594878f8996fd0ba9654adc
galin-kostadinov <gk_1988@abv.bg>
2020-04-10 13:00:50 +0300

    added findByUsernameAndPassword()

--- JavaWeb/Basic/04. Java EE - JSF_JPA/JobOffersApp/src/main/java/app/service/UserService.java
+++ JavaWeb/Basic/04. Java EE - JSF_JPA/JobOffersApp/src/main/java/app/service/UserService.java
@@ -4,4 +4,6 @@ import app.domain.model.service.UserServiceModel;
 
 public interface UserService {
     void save(UserServiceModel user);
+
+     UserServiceModel findByUsernameAndPassword(String username, String password);
 }
