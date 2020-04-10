046f9198181028cc7ae39355d088874f5e3b2873
galin-kostadinov <gk_1988@abv.bg>
2020-04-10 13:00:11 +0300

    added findByUsernameAndPassword()

--- JavaWeb/Basic/04. Java EE - JSF_JPA/JobOffersApp/src/main/java/app/repository/UserRepositoryImpl.java
+++ JavaWeb/Basic/04. Java EE - JSF_JPA/JobOffersApp/src/main/java/app/repository/UserRepositoryImpl.java
@@ -4,6 +4,7 @@ import app.domain.entity.User;
 
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import java.util.List;
 
 public class UserRepositoryImpl implements UserRepository {
     private final EntityManager entityManager;
@@ -18,5 +19,18 @@ public class UserRepositoryImpl implements UserRepository {
         this.entityManager.getTransaction().begin();
         this.entityManager.persist(user);
         this.entityManager.getTransaction().commit();
+    }
+
+    @Override
+    public User findByUsernameAndPassword(String username, String password) {
+        this.entityManager.getTransaction().begin();
+        List<User> users = this.entityManager.createQuery("select u from User u " +
+                "where u.username =:username and u.password=:password", User.class)
+                .setParameter("username", username)
+                .setParameter("password", password)
+                .getResultList();
+
+        this.entityManager.getTransaction().commit();
+        return users.isEmpty() ? null : users.get(0);
     }
 }
