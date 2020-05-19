e532ec740ec683851d945d4561bd9c3cb3580fa3
galin-kostadinov <gk_1988@abv.bg>
2020-05-19 17:37:40 +0300

    refactored

--- JS/JSAdvanced/01. Syntax, Functions and Statements/Exercise/01-fruit.js
+++ JS/JSAdvanced/01. Syntax, Functions and Statements/Exercise/01-fruit.js
@@ -1,10 +1,10 @@
 "use strict";
 
-function solve(name, weight, price) {
-    weight /= 1000;
-    const totalPrice = weight * price;
+function solve(name, weightInGrams, price) {
+    const weightInKilos = weightInGrams / 1000;
+    const totalPrice = weightInKilos * price;
 
-    console.log(`I need $${totalPrice.toFixed(2)} to buy ${weight.toFixed(2)} kilograms ${name}.`);
+    console.log(`I need $${totalPrice.toFixed(2)} to buy ${weightInKilos.toFixed(2)} kilograms ${name}.`);
 }
 
 solve('orange', 2500, 1.80);
