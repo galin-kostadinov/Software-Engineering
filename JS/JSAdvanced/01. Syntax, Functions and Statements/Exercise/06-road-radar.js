6342d22ffee0e11b72df49fa1c484d3fa8053376
galin-kostadinov <gk_1988@abv.bg>
2020-05-19 17:38:22 +0300

    added additional commented solution

--- JS/JSAdvanced/01. Syntax, Functions and Statements/Exercise/06-road-radar.js
+++ JS/JSAdvanced/01. Syntax, Functions and Statements/Exercise/06-road-radar.js
@@ -35,5 +35,23 @@ function solve(arr) {
     }
 }
 
+// function radar(input) {
+//     let speed = input[0];
+//     let area = input[1];
+ 
+//     const limits = {motorway: 130, interstate: 90, city: 50, residential: 20};
+ 
+//     let printMessage = (speed, limit) => {
+//         if (speed > limit + 40) {
+//             console.log('reckless driving');
+//         } else if (speed > limit + 20) {
+//             console.log('excessive speeding ');
+//         } else if (speed > limit) {
+//             console.log('speeding');
+//         }
+//     };
+//     printMessage(speed, limits[area]);
+// }
+
 solve([40, 'city']);
 solve([21, 'residential']);
--- JS/JSAdvanced/01. Syntax, Functions and Statements/Exercise/07-cooking-by-numbers.js
+++ JS/JSAdvanced/01. Syntax, Functions and Statements/Exercise/07-cooking-by-numbers.js
@@ -20,5 +20,23 @@ function solve(arr) {
     }
 }
 
+// function cookingByNumbers(input) {
+//     let number = Number(input.shift());
+ 
+//     const functions = {
+//         chop: (x) => x / 2,
+//         dice: (x) => Math.sqrt(x),
+//         spice: (x) => x + 1,
+//         bake: (x) => x * 3,
+//         fillet: (x) => x * 0.8,
+//     };
+ 
+//     input.forEach(function (element) {
+//         let result = functions[element](number);
+//         console.log(result);
+//         number = result;
+//     });
+// }
+
 solve(['32', 'chop', 'chop', 'chop', 'chop', 'chop']);
 solve(['9', 'dice', 'spice', 'chop', 'bake', 'fillet']);
