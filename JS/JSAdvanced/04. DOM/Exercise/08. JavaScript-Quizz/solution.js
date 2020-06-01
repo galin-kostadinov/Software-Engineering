26cc36cf58632238001c3c75717590a3f2f78eb6
galin-kostadinov <gk_1988@abv.bg>
2020-06-01 20:10:51 +0300

    implement the function

--- JS/JSAdvanced/04. DOM/Exercise/08. JavaScript-Quizz/solution.js
+++ JS/JSAdvanced/04. DOM/Exercise/08. JavaScript-Quizz/solution.js
@@ -1,3 +1,45 @@
 function solve() {
-  //TODO...
+  const answersLi = document.querySelectorAll('.answer-wrap');
+
+  for (let answer of answersLi) {
+    answer.addEventListener('click', hendler);
+  }
+
+  let index = 1;
+  let rightAnswers = 0;
+
+  function hendler(ev) {
+    const currAnswer = ev.target.textContent.trim();
+
+    if (index == 1) {
+      if (currAnswer === 'onclick') {
+
+        rightAnswers++;
+      }
+    } else if (index == 2) {
+      if (currAnswer === 'JSON.stringify()') {
+        rightAnswers++;
+      }
+    } else if (index == 3) {
+      if (currAnswer === 'A programming API for HTML and XML documents') {
+        rightAnswers++;
+      }
+    }
+
+    document.querySelector(`#quizzie section:nth-child(${index + 1})`).style.cssText = `display: none`;
+
+    if (index == 3) {
+      console.log(rightAnswers);
+      document.getElementById('results').style.display = 'block';
+      rightAnswers === 3
+        ? document.querySelector('#results h1').textContent = 'You are recognized as top JavaScript fan!'
+        : document.querySelector('#results h1').textContent = `You have ${rightAnswers} right answers`
+
+      return;
+    }
+
+    index++;
+    document.querySelector(`#quizzie section:nth-child(${index + 1})`).classList[0] = '';
+    document.querySelector(`#quizzie section:nth-child(${index + 1})`).style.cssText = `display: block`;
+  }
 }
--- JS/JSAdvanced/04. DOM/Exercise/09. Shopping-Cart/solution.js
+++ JS/JSAdvanced/04. DOM/Exercise/09. Shopping-Cart/solution.js
@@ -1,3 +1,37 @@
 function solve() {
-   //TODO...
+   const addButtons = document.querySelectorAll('.product-add button');
+   const checkoutButton = document.querySelector('button.checkout');
+   const shoppingCart = document.querySelector('.shopping-cart textarea');
+   let totalPrice = 0;
+   const products = new Set();
+   let isCheckout = false;
+   
+   for (let button of addButtons) {
+      button.addEventListener('click', addInCart);
+   }
+
+   checkoutButton.addEventListener('click', checkout);
+
+   function addInCart(ev) {
+      if (isCheckout) {
+         return;
+      }
+      const productDiv = ev.target.parentElement.parentElement;
+      let productTitle = productDiv.querySelector('.product-title').textContent;
+      let productPrice = productDiv.querySelector('.product-line-price').textContent;
+      totalPrice += Number(productPrice);
+      products.add(productTitle);
+
+      shoppingCart.value += `Added ${productTitle} for ${productPrice} to the cart.\n`;
+   }
+
+   function checkout(ev) {
+      if (isCheckout) {
+         return;
+      }
+
+      shoppingCart.value += `You bought ${Array.from(products).join(', ')} for ${totalPrice.toFixed(2)}.\n`;
+
+      isCheckout = true;
+   }
 }
