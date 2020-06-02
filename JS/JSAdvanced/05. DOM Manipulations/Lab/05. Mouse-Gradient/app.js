04f1aab63308cf53f12eb7059f8eb661c7cb80c8
galin-kostadinov <gk_1988@abv.bg>
2020-06-02 13:58:16 +0300

    implemented attachGradientEvents function

--- JS/JSAdvanced/05. DOM Manipulations/Lab/05. Mouse-Gradient/app.js
+++ JS/JSAdvanced/05. DOM Manipulations/Lab/05. Mouse-Gradient/app.js
@@ -1,3 +1,17 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    const gradient = document.querySelector('#gradient');
+    const result = document.querySelector('#result');
+
+    gradient.addEventListener('mousemove', gradientMove);
+    gradient.addEventListener('mouseleave', gradientOut);
+
+    function gradientMove(ev) {
+        let power = ev.offsetX / 3;
+        power = Math.floor(power);
+        result.textContent = power + "%";
+    }
+
+    function gradientOut(ev) {
+        result.textContent = '';
+    }
 }
