015ac98a5b33c135bc3624cc94fb357994a7ecc6
galin-kostadinov <gk_1988@abv.bg>
2020-06-02 17:52:21 +0300

    implemented validate function

--- JS/JSAdvanced/05. DOM Manipulations/Lab/07. Dynamic-Validation/app.js
+++ JS/JSAdvanced/05. DOM Manipulations/Lab/07. Dynamic-Validation/app.js
@@ -1,3 +1,16 @@
 function validate() {
-    console.log('TODO:...');
+    const regex = /[a-z\.]+@[a-z]+.[a-z]+/g;
+
+    const inputHTML = document.querySelector('input');
+    inputHTML.addEventListener('change', change);
+   
+    function change(ev) {
+        let imputEmail = ev.target.value;
+
+        if (regex.exec(imputEmail) !== null) {
+            ev.target.classList.remove('error');
+        } else {
+            ev.target.classList.add('error');
+        }
+    }
 }
