6419d5d09bb42c62b577f49934cb84b1fd6a1a20
galin-kostadinov <gk_1988@abv.bg>
2020-06-02 13:11:42 +0300

    implemented stopwatch function

--- JS/JSAdvanced/05. DOM Manipulations/Lab/04. Stopwatch/stopwatch.js
+++ JS/JSAdvanced/05. DOM Manipulations/Lab/04. Stopwatch/stopwatch.js
@@ -1,3 +1,36 @@
 function stopwatch() {
-    console.log('TODO:...');
+    const startBtn = document.querySelector('#startBtn');
+    const stopBtn = document.querySelector('#stopBtn');
+    const timeDiv = document.querySelector('#time');
+
+    startBtn.addEventListener('click', start);
+    stopBtn.addEventListener('click', stop);
+    let indervalID;
+    function start() {
+        startBtn.setAttribute('disabled', 'true');
+        stopBtn.removeAttribute('disabled');
+        timeDiv.textContent = '00:00';
+        let currentTime = timeDiv.textContent.split(':').map(el => Number(el));
+
+        indervalID = setInterval(
+            function () {
+                currentTime[1]++;
+
+                if (currentTime[1] === 60) {
+                    currentTime[1] = 0;
+                    currentTime[0]++;
+                }
+              
+                let time = `${currentTime[0].toString().padStart(2, "0")}:${currentTime[1].toString().padStart(2, "0")}`;
+                timeDiv.textContent = time;
+              
+            }, 1000
+        );
+    }
+
+    function stop() {
+        clearInterval(indervalID);
+        startBtn.removeAttribute('disabled');
+        stopBtn.setAttribute('disabled', 'true');
+    }
 }
