7c693efb2fa98fdc460f8fb1f94d371499f75cab
galin-kostadinov <gk_1988@abv.bg>
2020-06-02 13:58:43 +0300

    implemented focus function

--- JS/JSAdvanced/05. DOM Manipulations/Lab/06. Highlight-Active/app.js
+++ JS/JSAdvanced/05. DOM Manipulations/Lab/06. Highlight-Active/app.js
@@ -1,3 +1,16 @@
 function focus() {
-    console.log('TODO:...');
+    const divs = document.querySelectorAll('input');
+
+    for (let div of divs) {
+        div.addEventListener('focus', setFocuced);
+        div.addEventListener('blur', removeFocuced);
+    }
+
+    function setFocuced(ev) {
+        ev.target.parentNode.classList.add('focused');
+    }
+
+    function removeFocuced(ev) {
+        ev.target.parentNode.classList.remove('focused');
+    }
 }
