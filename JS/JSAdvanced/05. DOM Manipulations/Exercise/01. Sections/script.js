7fdb558cea3ec2b5b8070ee993baf5d4b15f6472
galin-kostadinov <gk_1988@abv.bg>
2020-06-03 13:24:22 +0300

    implemented function create(words){}

--- JS/JSAdvanced/05. DOM Manipulations/Exercise/01. Sections/script.js
+++ JS/JSAdvanced/05. DOM Manipulations/Exercise/01. Sections/script.js
@@ -1,3 +1,20 @@
 function create(words) {
-   console.log('TODO:...');
+   const content = document.querySelector('#content');
+
+   for (const word of words) {
+      let currDiv = document.createElement('div');
+      currDiv.addEventListener('click', showTextContent)
+
+      let currP = document.createElement('p');
+      currP.style.display = 'none';
+      currP.textContent = word;
+
+      currDiv.appendChild(currP);
+
+      content.appendChild(currDiv);
+   }
+
+   function showTextContent(ev) {
+      ev.target.childNodes[0].style.display = 'block';
+   }
 }
