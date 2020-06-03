93626a3c9afec1a9d4d84b46cb7a44473418eace
galin-kostadinov <gk_1988@abv.bg>
2020-06-03 13:37:04 +0300

    implemented function notify(message){}

--- JS/JSAdvanced/05. DOM Manipulations/Exercise/02. Notification/scripts.js
+++ JS/JSAdvanced/05. DOM Manipulations/Exercise/02. Notification/scripts.js
@@ -1,3 +1,8 @@
 function notify(message) {
-    console.log('TODO:...');
+    const notificationDiv = document.querySelector('#notification');
+
+    notificationDiv.textContent = message;
+    notificationDiv.style.display = 'block';
+
+    setTimeout(function () { notificationDiv.style.display = 'none'; }, 2000);
 }
