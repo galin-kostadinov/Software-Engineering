dd3a1f42b7a80770abc5f61446aeeb7fe5e3b19c
galin-kostadinov <gk_1988@abv.bg>
2020-06-04 16:33:54 +0300

    implemented function lockedProfile()

--- JS/JSAdvanced/05. DOM Manipulations/Exercise/04. Locked-Profile/app.js
+++ JS/JSAdvanced/05. DOM Manipulations/Exercise/04. Locked-Profile/app.js
@@ -1,3 +1,27 @@
 function lockedProfile() {
-    console.log('TODO...')
+    let buttens = document.querySelectorAll('#main button');
+    for (let button of buttens) {
+        button.addEventListener('click', showMore);
+    }
+
+    function showMore(ev) {
+        const profileDiv = ev.target.parentElement;
+        const userHiddenFields = profileDiv.querySelector('div');
+        const radioLock = profileDiv.querySelector("input[type='radio'][value='lock']").checked;
+        const radioUnlock = profileDiv.querySelector("input[type='radio'][value='unlock']").checked;
+
+        let textContent = ev.target.textContent;
+
+        if (radioLock) {
+            return;
+        }
+
+        if (radioUnlock && textContent == 'Show more') {
+            userHiddenFields.style.display = 'block';
+            ev.target.textContent = 'Hide it';
+        } else if (radioUnlock && textContent == 'Hide it') {
+            userHiddenFields.style.display = 'none';
+            ev.target.textContent = 'Show more';
+        }
+    }
 }
