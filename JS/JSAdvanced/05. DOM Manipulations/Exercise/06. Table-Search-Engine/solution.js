65da44632df72fa0dcda11262fdf6817cbc58fe7
galin-kostadinov <gk_1988@abv.bg>
2020-06-04 16:34:46 +0300

    implemented function solve()

--- JS/JSAdvanced/05. DOM Manipulations/Exercise/06. Table-Search-Engine/solution.js
+++ JS/JSAdvanced/05. DOM Manipulations/Exercise/06. Table-Search-Engine/solution.js
@@ -1,3 +1,24 @@
 function solve() {
-   //TODO...
+   const result = document.querySelector('#result');
+
+   document.querySelector('#searchBtn').addEventListener('click', search);
+   let input = document.querySelector('#searchField');
+
+   const allRows = document.querySelectorAll('tbody tr')
+
+   function search(ev) {
+      for (let row of allRows) {
+         row.classList.remove('select');
+      }
+
+      let value = input.value;
+
+      for (let row of allRows) {
+         if (row.textContent.includes(value)) {
+            row.classList.add('select');
+         }
+      }
+
+      input.value = '';
+   }
 }
