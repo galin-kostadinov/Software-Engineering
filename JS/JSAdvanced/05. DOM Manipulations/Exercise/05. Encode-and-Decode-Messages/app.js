eb1c3fe25a928a3ec8ea25d5c23011d94ec24616
galin-kostadinov <gk_1988@abv.bg>
2020-06-04 16:34:16 +0300

    implemented function encodeAndDecodeMessages()

--- JS/JSAdvanced/05. DOM Manipulations/Exercise/05. Encode-and-Decode-Messages/app.js
+++ JS/JSAdvanced/05. DOM Manipulations/Exercise/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,21 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    const encodeBtn = document.querySelectorAll('button')[0];
+    const decodeBtn = document.querySelectorAll('button')[1];
+    const sendArea = encodeBtn.parentNode.querySelector('textarea');
+    const receiveArea = decodeBtn.parentNode.querySelector('textarea');
+
+    document.addEventListener('click', handler);
+
+    function handler(ev) {
+        const { content, mapChar } = ev.target.textContent.includes('Encode')
+            ? { content: sendArea.value, mapChar: x => x.charCodeAt() + 1 }
+            : { content: receiveArea.value, mapChar: x => x.charCodeAt() - 1 };
+
+        receiveArea.value = content
+            .split('')
+            .map(char => mapChar(char))
+            .map(ascii => String.fromCharCode(ascii))
+            .join('');
+        sendArea.value = '';
+    }
 }
