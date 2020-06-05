8beaca9b4afbb043d10ef7cb1dc9660972e03056
galin-kostadinov <gk_1988@abv.bg>
2020-06-05 09:45:03 +0300

    implemented function solve()

--- JS/JSAdvanced/05. DOM Manipulations/Exercise/10. Sudomu/solution.js
+++ JS/JSAdvanced/05. DOM Manipulations/Exercise/10. Sudomu/solution.js
@@ -1,4 +1,73 @@
 function solve() {
+    const buttens = document.querySelectorAll('button');
+    const quickCheckButton = buttens[0];
+    const clearButton = buttens[1];
 
-    //TODO...
+    quickCheckButton.addEventListener('click', quickCheck);
+    clearButton.addEventListener('click', clear);
+
+    const inputs = document.querySelectorAll('input');
+    const checkDiv = document.querySelector('#check p');
+    const table = document.querySelector('#exercise>table');
+
+    function quickCheck(ev) {
+        let matrix = fillInputMatrix();
+
+        if (checkIsRigth(matrix)) {
+            table.style.border = '2px solid green';
+            checkDiv.textContent = 'You solve it! Congratulations!';
+            checkDiv.style.color = 'green';
+        } else {
+            table.style.border = '2px solid red';
+            checkDiv.textContent = 'NOP! You are not done yet...';
+            checkDiv.style.color = 'red';
+        }
+    }
+
+    function clear(ev) {
+        table.style.border = '';
+        checkDiv.textContent = '';
+
+        for (let input of inputs) {
+            input.value = '';
+        }
+    }
+
+    function fillInputMatrix() {
+        let size = Math.sqrt(inputs.length);
+        let inputIndex = 0;
+        const matrix = [];
+        for (let row = 0; row < size; row++) {
+            const array = [];
+            for (let col = 0; col < size; col++) {
+                array.push(Number(inputs[inputIndex++].value));
+            }
+            matrix.push(array);
+        }
+
+        return matrix;
+    }
+
+    function checkIsRigth(matrix) {
+        let size = matrix.length;
+
+        for (let row = 0; row < size; row++) {
+            const set = new Set(matrix[row]);
+            if (set.size !== size || set.has(0)) {
+                return false;
+            }
+        }
+
+        for (let col = 0; col < size; col++) {
+            const set = new Set();
+            for (let row = 0; row < size; row++) {
+                set.add(matrix[row][col]);
+            }
+            if (set.size !== size) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
